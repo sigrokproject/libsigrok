204b1629bd4ef6b61c297fd830a3dae9edafc388
Uwe Hermann <uwe@hermann-uwe.de>
2010-04-27 22:46:59 +0200

    asix-sigma.h: Add missing license header.

--- hardware/asix-sigma/asix-sigma.h
+++ hardware/asix-sigma/asix-sigma.h
@@ -1,3 +1,24 @@
+/*
+ * This file is part of the sigrok project.
+ *
+ * Copyright (C) 2010 Håvard Espeland <gus@ping.uio.no>,
+ * Copyright (C) 2010 Martin Stensgård <mastensg@ping.uio.no>
+ * Copyright (C) 2010 Carl Henrik Lunde <chlunde@ping.uio.no>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef ASIX_SIGMA_H
 #define ASIX_SIGMA_H
 
@@ -52,6 +73,4 @@ enum sigma_read_register
 
 #define CHUNK_SIZE		1024
 
-#endif /* ASIX_SIGMA_H */
-
-// vim:noexpandtab:ts=8 sts=8 sw=8
+#endif
