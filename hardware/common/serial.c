2f3aab0ee9797700238656218208a6ab17a607b3
Uwe Hermann <uwe@hermann-uwe.de>
2011-04-03 22:38:35 +0200

    Disable some stuff which should not be in 0.2.
    Disable decoders, disable lib building.

--- hardware/common/serial.c
+++ hardware/common/serial.c
@@ -243,9 +243,11 @@ int serial_set_params(int fd, int speed, int bits, int parity, int stopbits,
 	case 115200:
 		baud = B115200;
 		break;
+#if 0
 	case 460800:
 		baud = B460800;
 		break;
+#endif
 	default:
 		return SR_ERR;
 	}
