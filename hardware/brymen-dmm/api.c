20cbc7854dc0a375e23fea2ca7e7feee3d3ac701
Alexandru Gagniuc <mr.nuke.me@gmail.com>
2012-11-21 21:20:39 -0600

    brymen-dmm: Initial driver skeleton.

---
+++ hardware/brymen-dmm/api.c
@@ -0,0 +1,193 @@
+/*
+ * This file is part of the libsigrok project.
+ *
+ * Copyright (C) 2012 Alexandru Gagniuc <mr.nuke.me@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <glib.h>
+#include "libsigrok.h"
+#include "libsigrok-internal.h"
+#include "protocol.h"
+
+SR_PRIV struct sr_dev_driver brymen_dmm_driver_info;
+static struct sr_dev_driver *di = &brymen_dmm_driver_info;
+
+/* Properly close and free all devices. */
+static int clear_instances(void)
+{
+	struct sr_dev_inst *sdi;
+	struct drv_context *drvc;
+	struct dev_context *devc;
+	GSList *l;
+
+	if (!(drvc = di->priv))
+		return SR_OK;
+
+	for (l = drvc->instances; l; l = l->next) {
+		if (!(sdi = l->data))
+			continue;
+		if (!(devc = sdi->priv))
+			continue;
+
+		/* TODO */
+
+		sr_dev_inst_free(sdi);
+	}
+
+	g_slist_free(drvc->instances);
+	drvc->instances = NULL;
+
+	return SR_OK;
+}
+
+static int hw_init(void)
+{
+	struct drv_context *drvc;
+
+	if (!(drvc = g_try_malloc0(sizeof(struct drv_context)))) {
+		sr_err("Driver context malloc failed.");
+		return SR_ERR_MALLOC;
+	}
+
+	/* TODO */
+
+	di->priv = drvc;
+
+	return SR_OK;
+}
+
+static GSList *hw_scan(GSList *options)
+{
+	struct drv_context *drvc;
+	GSList *devices;
+
+	(void)options;
+
+	devices = NULL;
+	drvc = di->priv;
+	drvc->instances = NULL;
+
+	/* TODO */
+
+	return devices;
+}
+
+static GSList *hw_dev_list(void)
+{
+	struct drv_context *drvc;
+
+	drvc = di->priv;
+
+	return drvc->instances;
+}
+
+static int hw_dev_open(struct sr_dev_inst *sdi)
+{
+	/* TODO */
+
+	return SR_OK;
+}
+
+static int hw_dev_close(struct sr_dev_inst *sdi)
+{
+	/* TODO */
+
+	return SR_OK;
+}
+
+static int hw_cleanup(void)
+{
+	clear_instances();
+
+	/* TODO */
+
+	return SR_OK;
+}
+
+static int hw_info_get(int info_id, const void **data,
+		       const struct sr_dev_inst *sdi)
+{
+	switch (info_id) {
+	/* TODO */
+	default:
+		sr_err("Unknown info_id: %d.", info_id);
+		return SR_ERR_ARG;
+	}
+
+	return SR_OK;
+}
+
+static int hw_dev_config_set(const struct sr_dev_inst *sdi, int hwcap,
+			     const void *value)
+{
+	int ret;
+
+	if (sdi->status != SR_ST_ACTIVE) {
+		sr_err("Device inactive, can't set config options.");
+		return SR_ERR;
+	}
+
+	ret = SR_OK;
+	switch (hwcap) {
+	/* TODO */
+	default:
+		sr_err("Unknown hardware capability: %d.", hwcap);
+		ret = SR_ERR_ARG;
+	}
+
+	return ret;
+}
+
+static int hw_dev_acquisition_start(const struct sr_dev_inst *sdi,
+				    void *cb_data)
+{
+	/* TODO */
+
+	return SR_OK;
+}
+
+static int hw_dev_acquisition_stop(const struct sr_dev_inst *sdi,
+				   void *cb_data)
+{
+	(void)cb_data;
+
+	if (sdi->status != SR_ST_ACTIVE) {
+		sr_err("Device inactive, can't stop acquisition.");
+		return SR_ERR;
+	}
+
+	/* TODO */
+
+	return SR_OK;
+}
+
+SR_PRIV struct sr_dev_driver brymen_dmm_driver_info = {
+	.name = "brymen-dmm",
+	.longname = "brymen-dmm",
+	.api_version = 1,
+	.init = hw_init,
+	.cleanup = hw_cleanup,
+	.scan = hw_scan,
+	.dev_list = hw_dev_list,
+	.dev_clear = clear_instances,
+	.dev_open = hw_dev_open,
+	.dev_close = hw_dev_close,
+	.info_get = hw_info_get,
+	.dev_config_set = hw_dev_config_set,
+	.dev_acquisition_start = hw_dev_acquisition_start,
+	.dev_acquisition_stop = hw_dev_acquisition_stop,
+	.priv = NULL,
+};
--- hwdriver.c
+++ hwdriver.c
@@ -83,6 +83,9 @@ static struct sr_config_info sr_config_info_data[] = {
 };
 
 /** @cond PRIVATE */
+#ifdef HAVE_HW_BRYMEN_DMM
+extern SR_PRIV struct sr_dev_driver brymen_dmm_driver_info;
+#endif
 #ifdef HAVE_HW_COLEAD_SLM
 extern SR_PRIV struct sr_dev_driver colead_slm_driver_info;
 #endif
@@ -160,6 +163,9 @@ extern SR_PRIV struct sr_dev_driver voltcraft_vc820_driver_info;
 /** @endcond */
 
 static struct sr_dev_driver *drivers_list[] = {
+#ifdef HAVE_HW_BRYMEN_DMM
+	&brymen_dmm_driver_info,
+#endif
 #ifdef HAVE_HW_COLEAD_SLM
 	&colead_slm_driver_info,
 #endif
