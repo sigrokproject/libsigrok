fdf4a1f5a0a04046ec014786b4a8fce125e1163c
Uwe Hermann <uwe@hermann-uwe.de>
2013-06-21 17:15:10 +0200

    ikalogic-scanaplus: Initial driver skeleton.

--- hwdriver.c
+++ hwdriver.c
@@ -116,6 +116,9 @@ extern SR_PRIV struct sr_dev_driver demo_driver_info;
 #ifdef HAVE_HW_IKALOGIC_SCANALOGIC2
 extern SR_PRIV struct sr_dev_driver ikalogic_scanalogic2_driver_info;
 #endif
+#ifdef HAVE_HW_IKALOGIC_SCANAPLUS
+extern SR_PRIV struct sr_dev_driver ikalogic_scanaplus_driver_info;
+#endif
 #ifdef HAVE_HW_LASCAR_EL_USB
 extern SR_PRIV struct sr_dev_driver lascar_el_usb_driver_info;
 #endif
@@ -217,6 +220,9 @@ static struct sr_dev_driver *drivers_list[] = {
 #endif
 #ifdef HAVE_HW_IKALOGIC_SCANALOGIC2
 	&ikalogic_scanalogic2_driver_info,
+#endif
+#ifdef HAVE_HW_IKALOGIC_SCANAPLUS
+	&ikalogic_scanaplus_driver_info,
 #endif
 #ifdef HAVE_HW_LASCAR_EL_USB
 	&lascar_el_usb_driver_info,
