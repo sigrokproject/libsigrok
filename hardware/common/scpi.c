ed840c8612a42506e7c28cca83b4586d9bd79471
Uwe Hermann <uwe@hermann-uwe.de>
2014-02-07 19:01:45 +0100

    scpi: Disable scpi_usbtmc in favor of scpi_usbtmc_libusb for now.
    Having both in the list will "find" the same device twice.

--- hardware/common/scpi.c
+++ hardware/common/scpi.c
@@ -68,7 +68,7 @@ static int parse_strict_bool(const char *str, gboolean *ret)
 SR_PRIV extern const struct sr_scpi_dev_inst scpi_serial_dev;
 SR_PRIV extern const struct sr_scpi_dev_inst scpi_tcp_raw_dev;
 SR_PRIV extern const struct sr_scpi_dev_inst scpi_tcp_rigol_dev;
-SR_PRIV extern const struct sr_scpi_dev_inst scpi_usbtmc_dev;
+/* SR_PRIV extern const struct sr_scpi_dev_inst scpi_usbtmc_dev; */
 SR_PRIV extern const struct sr_scpi_dev_inst scpi_usbtmc_libusb_dev;
 SR_PRIV extern const struct sr_scpi_dev_inst scpi_vxi_dev;
 SR_PRIV extern const struct sr_scpi_dev_inst scpi_visa_dev;
@@ -76,7 +76,7 @@ SR_PRIV extern const struct sr_scpi_dev_inst scpi_visa_dev;
 static const struct sr_scpi_dev_inst *scpi_devs[] = {
 	&scpi_tcp_raw_dev,
 	&scpi_tcp_rigol_dev,
-	&scpi_usbtmc_dev,
+	/* &scpi_usbtmc_dev, */
 #ifdef HAVE_LIBUSB_1_0
 	&scpi_usbtmc_libusb_dev,
 #endif
