e0b781a45b5c6d13d3947c50571ddf22ee455e30
Martin Ling <martin-git@earth.li>
2014-09-16 02:07:08 +0100

    agilent-dmm: Mark serial read call as nonblocking.
    This driver opens the port with the SERIAL_NONBLOCK flag, so this call is
    already a nonblocking one, and is handled appropriately.

--- src/hardware/agilent-dmm/sched.c
+++ src/hardware/agilent-dmm/sched.c
@@ -106,7 +106,7 @@ SR_PRIV int agdmm_receive_data(int fd, int revents, void *cb_data)
 	if (revents == G_IO_IN) {
 		/* Serial data arrived. */
 		while(AGDMM_BUFSIZE - devc->buflen - 1 > 0) {
-			len = serial_read(serial, devc->buf + devc->buflen, 1);
+			len = serial_read_nonblocking(serial, devc->buf + devc->buflen, 1);
 			if (len < 1)
 				break;
 			devc->buflen += len;
