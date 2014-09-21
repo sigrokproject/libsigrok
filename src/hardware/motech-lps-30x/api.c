856dccb7a6af99dbc4955053daca1b219bd6931d
Martin Ling <martin-git@earth.li>
2014-09-21 19:03:26 +0100

    motech-lps-30x: Make serial write call block.
    This call was previously explicitly nonblocking, but has no partial write
    handling. It sends a short packet so should be OK to block, most likely the
    output buffer will be empty anyway.

--- src/hardware/motech-lps-30x/api.c
+++ src/hardware/motech-lps-30x/api.c
@@ -146,7 +146,7 @@ SR_PRIV int lps_send_va(struct sr_serial_dev_inst *serial, const char* fmt, va_l
 
 	sr_spew("lps_send_va: \"%s\"", buf);
 
-	retc = serial_write_nonblocking(serial, buf, strlen(buf));
+	retc = serial_write_blocking(serial, buf, strlen(buf));
 
 	if (retc < 0)
 		return SR_ERR;
