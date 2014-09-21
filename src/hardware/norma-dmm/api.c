d12ef776d3e81f30ca75bbdab7ea96341ad930a4
Martin Ling <martin-git@earth.li>
2014-09-21 19:14:49 +0100

    norma-dmm: Remove SERIAL_NONBLOCK flag.
    All calls in the driver are now explicitly (non)blocking.

--- src/hardware/norma-dmm/api.c
+++ src/hardware/norma-dmm/api.c
@@ -115,7 +115,7 @@ static GSList *do_scan(struct sr_dev_driver* drv, GSList *options)
 	if (!(serial = sr_serial_dev_inst_new(conn, serialcomm)))
 		return NULL;
 
-	if (serial_open(serial, SERIAL_RDWR | SERIAL_NONBLOCK) != SR_OK)
+	if (serial_open(serial, SERIAL_RDWR) != SR_OK)
 		return NULL;
 
 	serial_flush(serial);
