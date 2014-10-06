76bc5f6376104e8b65322011b67aafbd45348fe2
Aurelien Jacobs <aurel@gnuage.org>
2014-10-07 00:02:10 +0200

    usb: Simplify usb_get_port_path() and fix writing into a const char *.

--- src/usb.c
+++ src/usb.c
@@ -271,27 +271,18 @@ SR_PRIV int usb_source_remove(struct sr_session *session, struct sr_context *ctx
 	return SR_OK;
 }
 
-SR_PRIV int usb_get_port_path(libusb_device *dev, const char *path, int path_len)
+SR_PRIV int usb_get_port_path(libusb_device *dev, char *path, int path_len)
 {
-	gchar s[64];
-	gchar tmp_s[8];
-	uint8_t port_numbers[32];
-	int i;
+	uint8_t port_numbers[8];
+	int i, n, len;
 
-	memset(port_numbers, 0, sizeof(port_numbers));
-	libusb_get_port_numbers(dev, port_numbers, sizeof(port_numbers));
+	n = libusb_get_port_numbers(dev, port_numbers, sizeof(port_numbers));
 
-	g_snprintf((gchar*)&s, sizeof(s), "usb/%d-%d",
-			libusb_get_bus_number(dev), port_numbers[0]);
+	len = snprintf(path, path_len, "usb/%d-%d",
+	               libusb_get_bus_number(dev), port_numbers[0]);
 
-	i = 1;
-	while (port_numbers[i]) {
-		g_snprintf(tmp_s, sizeof(tmp_s), ".%d", port_numbers[i]);
-		g_strlcat((gchar*)&s, tmp_s, sizeof(s));
-		i++;
-	}
-
-	g_strlcpy((gchar*)path, s, path_len);
+	for (i = 1; i < n; i++)
+		len += snprintf(path+len, path_len-len, ".%d", port_numbers[i]);
 
 	return SR_OK;
 }
