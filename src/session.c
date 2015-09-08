1e7659609bddd40471faf9a8e81ee7554d77dce9
Bert Vermeulen <bert@biot.com>
2015-09-08 14:25:10 +0200

    Don't free trigger when destroying the session.
    The trigger must be freed by the client.

--- src/session.c
+++ src/session.c
@@ -125,8 +125,6 @@ SR_API int sr_session_destroy(struct sr_session *session)
 
 	sr_session_dev_remove_all(session);
 	g_mutex_clear(&session->stop_mutex);
-	if (session->trigger)
-		sr_trigger_free(session->trigger);
 
 	g_slist_free_full(session->owned_devs, (GDestroyNotify)sr_dev_inst_free);
 
