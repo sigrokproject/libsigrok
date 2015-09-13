8b9eb639b2e12ab2f9d710af7342f26283747904
Uwe Hermann <uwe@hermann-uwe.de>
2015-09-13 23:55:22 +0200

    conrad-digi-35-cpu: Fix blocking serial write timeout.
    This fixes bug #430.

--- src/hardware/conrad-digi-35-cpu/protocol.c
+++ src/hardware/conrad-digi-35-cpu/protocol.c
@@ -49,7 +49,8 @@ SR_PRIV int send_msg1(const struct sr_dev_inst *sdi, char cmd, int param)
 
 	sr_spew("send_msg1(): %c%c%c%c\\r", buf[0], buf[1], buf[2], buf[3]);
 
-	if (serial_write_blocking(serial, buf, sizeof(buf), 0) < (int)sizeof(buf)) {
+	if (serial_write_blocking(serial, buf, sizeof(buf),
+			serial_timeout(serial, sizeof(buf))) < (int)sizeof(buf)) {
 		sr_err("Write error for cmd=%c", cmd);
 		return SR_ERR;
 	}
