2ea1fdf12113311cbe1a4316e9e2efe4d8ac40f0
Gerhard Sittig <gerhard.sittig@gmx.net>
2016-10-20 09:57:25 +0200

    license: remove FSF postal address from boiler plate license text
    Remove the FSF postal address as it might change (it did in the past).
    Reference the gnu.org website instead which is more stable.

--- src/hardware/arachnid-labs-re-load-pro/protocol.h
+++ src/hardware/arachnid-labs-re-load-pro/protocol.h
@@ -14,8 +14,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef LIBSIGROK_HARDWARE_ARACHNID_LABS_RE_LOAD_PRO_PROTOCOL_H
