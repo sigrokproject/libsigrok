ab2b21fb681a6dc3cfb12568886c6af9b8ffe6ff
JohnCh <subs@qcontinuum.plus.com>
2017-09-09 17:31:15 +0100

    fluke-45: Add initial driver implementation.

--- src/hardware/fluke-45/api.c
+++ src/hardware/fluke-45/api.c
@@ -1,6 +1,7 @@
 /*
  * This file is part of the libsigrok project.
  *
+ * Copyright (C) 2012 Bert Vermeulen <bert@biot.com>
  * Copyright (C) 2017 John Chajecki <subs@qcontinuum.plus.com>
  *
  * This program is free software: you can redistribute it and/or modify
@@ -18,120 +19,210 @@
  */
 
 #include <config.h>
+#include <glib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <libsigrok/libsigrok.h>
+#include "libsigrok-internal.h"
+#include "scpi.h"
 #include "protocol.h"
+
+static const uint32_t scanopts[] = {
+	SR_CONF_CONN,
+	SR_CONF_SERIALCOMM,
+};
+
+static const uint32_t drvopts[] = {
+	SR_CONF_MULTIMETER,
+};
+
+static const uint32_t devopts[] = {
+	SR_CONF_CONTINUOUS,
+	SR_CONF_LIMIT_SAMPLES | SR_CONF_SET,
+	SR_CONF_LIMIT_MSEC | SR_CONF_SET,
+};
+
+/* Vendor, model, number of channels, poll period */
+static const struct fluke_scpi_dmm_model supported_models[] = {
+	{ "FLUKE", "45", 2, 0 },
+};
+
+static struct sr_dev_driver fluke_45_driver_info;
+
+static struct sr_dev_inst *probe_device(struct sr_scpi_dev_inst *scpi)
+{
+	struct dev_context *devc;
+	struct sr_dev_inst *sdi;
+	struct sr_scpi_hw_info *hw_info;
+	const struct scpi_command *cmdset = fluke_45_cmdset;
+	unsigned int i;
+	const struct fluke_scpi_dmm_model *model = NULL;
+	gchar *channel_name;
+	char *response;
+
+	sdi = g_malloc0(sizeof(struct sr_dev_inst));
+	sdi->conn = scpi;
+
+	/* Test for serial port ECHO enabled. */
+	sr_scpi_get_string(scpi, "ECHO-TEST", &response);
+	if (strcmp(response, "ECHO-TEST") == 0) {
+		sr_err("Serial port ECHO is ON. Please turn it OFF!");
+		return NULL;
+	}
+
+	/* Get device IDN. */
+	if (sr_scpi_get_hw_id(scpi, &hw_info) != SR_OK) {
+		sr_info("Couldn't get IDN response, retrying.");
+		sr_scpi_close(scpi);
+		sr_scpi_open(scpi);
+		if (sr_scpi_get_hw_id(scpi, &hw_info) != SR_OK) {
+			sr_info("Couldn't get IDN response.");
+			return NULL;
+		}
+	}
+
+	/* Check IDN. */
+	for (i = 0; i < ARRAY_SIZE(supported_models); i++) {
+		if (!g_ascii_strcasecmp(hw_info->manufacturer,
+					supported_models[i].vendor) &&
+				!strcmp(hw_info->model, supported_models[i].model)) {
+			model = &supported_models[i];
+			break;
+		}
+	}
+	if (!model) {
+		sr_scpi_hw_info_free(hw_info);
+		return NULL;
+	}
+
+	/* Set up device parameters. */
+	sdi->vendor = g_strdup(model->vendor);
+	sdi->model = g_strdup(model->model);
+	sdi->version = g_strdup(hw_info->firmware_version);
+	sdi->serial_num = g_strdup(hw_info->serial_number);
+	sdi->conn = scpi;
+	sdi->driver = &fluke_45_driver_info;
+	sdi->inst_type = SR_INST_SCPI;
+
+	devc = g_malloc0(sizeof(struct dev_context));
+	devc->num_channels = model->num_channels;
+	devc->cmdset = cmdset;
+
+	/* Create channels. */
+	for (i = 0; i < devc->num_channels; i++) {
+		channel_name = g_strdup_printf("P%d", i + 1);
+		sr_channel_new(sdi, 0, SR_CHANNEL_ANALOG, TRUE, channel_name);
+	}
+
+	sdi->priv = devc;
+
+	return sdi;
+}
 
 static GSList *scan(struct sr_dev_driver *di, GSList *options)
 {
-	struct drv_context *drvc;
-	GSList *devices;
-
-	(void)options;
-
-	devices = NULL;
-	drvc = di->context;
-	drvc->instances = NULL;
-
-	/* TODO: scan for devices, either based on a SR_CONF_CONN option
-	 * or on a USB scan. */
-
-	return devices;
+	return sr_scpi_scan(di->context, options, probe_device);
 }
 
 static int dev_open(struct sr_dev_inst *sdi)
 {
-	(void)sdi;
+	struct sr_scpi_dev_inst *scpi;
+	int ret;
 
-	/* TODO: get handle from sdi->conn and open it. */
+	scpi = sdi->conn;
+
+	if ((ret = sr_scpi_open(scpi) < 0)) {
+		sr_err("Failed to open SCPI device: %s.", sr_strerror(ret));
+		return SR_ERR;
+	}
 
 	return SR_OK;
 }
 
 static int dev_close(struct sr_dev_inst *sdi)
 {
-	(void)sdi;
+	struct sr_scpi_dev_inst *scpi;
 
-	/* TODO: get handle from sdi->conn and close it. */
+	scpi = sdi->conn;
 
-	return SR_OK;
-}
+	if (!scpi)
+		return SR_ERR_BUG;
 
-static int config_get(uint32_t key, GVariant **data,
-	const struct sr_dev_inst *sdi, const struct sr_channel_group *cg)
-{
-	int ret;
-
-	(void)sdi;
-	(void)data;
-	(void)cg;
-
-	ret = SR_OK;
-	switch (key) {
-	/* TODO */
-	default:
-		return SR_ERR_NA;
-	}
-
-	return ret;
+	return sr_scpi_close(scpi);
 }
 
 static int config_set(uint32_t key, GVariant *data,
 	const struct sr_dev_inst *sdi, const struct sr_channel_group *cg)
 {
-	int ret;
+	struct dev_context *devc;
 
-	(void)sdi;
-	(void)data;
 	(void)cg;
 
-	ret = SR_OK;
-	switch (key) {
-	/* TODO */
-	default:
-		ret = SR_ERR_NA;
-	}
+	devc = sdi->priv;
 
-	return ret;
+	return sr_sw_limits_config_set(&devc->limits, key, data);
 }
 
 static int config_list(uint32_t key, GVariant **data,
 	const struct sr_dev_inst *sdi, const struct sr_channel_group *cg)
 {
-	int ret;
+	return STD_CONFIG_LIST(key, data, sdi, cg, scanopts, drvopts, devopts);
+}
 
-	(void)sdi;
-	(void)data;
+static int config_get(uint32_t key, GVariant **data,
+	const struct sr_dev_inst *sdi, const struct sr_channel_group *cg)
+{
+	struct dev_context *devc = sdi->priv;
+
 	(void)cg;
 
-	ret = SR_OK;
-	switch (key) {
-	/* TODO */
-	default:
-		return SR_ERR_NA;
-	}
-
-	return ret;
+	return sr_sw_limits_config_get(&devc->limits, key, data);
 }
 
 static int dev_acquisition_start(const struct sr_dev_inst *sdi)
 {
-	/* TODO: configure hardware, reset acquisition state, set up
-	 * callbacks and send header packet. */
+	struct sr_scpi_dev_inst *scpi;
+	struct dev_context *devc;
+	int ret;
 
-	(void)sdi;
+	scpi = sdi->conn;
+	devc = sdi->priv;
+
+	sr_sw_limits_acquisition_start(&devc->limits);
+	std_session_send_df_header(sdi);
+
+	if ((ret = sr_scpi_source_add(sdi->session, scpi, G_IO_IN, 10,
+			fl45_scpi_receive_data, (void *)sdi)) != SR_OK)
+		return ret;
 
 	return SR_OK;
 }
 
 static int dev_acquisition_stop(struct sr_dev_inst *sdi)
 {
-	/* TODO: stop acquisition. */
+	struct sr_scpi_dev_inst *scpi;
+	double d;
 
-	(void)sdi;
+	scpi = sdi->conn;
+
+	/*
+	 * A requested value is certainly on the way. Retrieve it now,
+	 * to avoid leaving the device in a state where it's not expecting
+	 * commands.
+	 */
+	sr_scpi_get_double(scpi, NULL, &d);
+	sr_scpi_source_remove(sdi->session, scpi);
+
+	std_session_send_df_end(sdi);
 
 	return SR_OK;
 }
 
-SR_PRIV struct sr_dev_driver fluke_45_driver_info = {
+static struct sr_dev_driver fluke_45_driver_info = {
 	.name = "fluke-45",
 	.longname = "Fluke 45",
 	.api_version = 1,
@@ -149,5 +240,4 @@ SR_PRIV struct sr_dev_driver fluke_45_driver_info = {
 	.dev_acquisition_stop = dev_acquisition_stop,
 	.context = NULL,
 };
-
 SR_REGISTER_DEV_DRIVER(fluke_45_driver_info);
--- src/hardware/fluke-45/protocol.h
+++ src/hardware/fluke-45/protocol.h
@@ -1,6 +1,7 @@
 /*
  * This file is part of the libsigrok project.
  *
+ * Copyright (C) 2012 Bert Vermeulen <bert@biot.com>
  * Copyright (C) 2017 John Chajecki <subs@qcontinuum.plus.com>
  *
  * This program is free software: you can redistribute it and/or modify
@@ -21,15 +22,179 @@
 #define LIBSIGROK_HARDWARE_FLUKE_45_PROTOCOL_H
 
 #include <stdint.h>
-#include <glib.h>
-#include <libsigrok/libsigrok.h>
-#include "libsigrok-internal.h"
+#include <stdbool.h>
+#include <scpi.h>
 
 #define LOG_PREFIX "fluke-45"
 
-struct dev_context {
+#define FLUKEDMM_BUFSIZE 256
+
+/* Always USB-serial, 1ms is plenty. */
+#define SERIAL_WRITE_TIMEOUT_MS 1
+
+enum data_format {
+	/* Fluke 45 uses IEEE488v2. */
+	FORMAT_IEEE488_2,
 };
 
-SR_PRIV int fluke_45_receive_data(int fd, int revents, void *cb_data);
+enum dmm_scpi_cmds {
+	SCPI_CMD_CLS,
+	SCPI_CMD_RST,
+	SCPI_CMD_REMS,
+	SCPI_CMD_RWLS,
+	SCPI_CMD_LOCS,
+	SCPI_CMD_LWLS,
+	SCPI_CMD_REMOTE,
+	SCPI_CMD_LOCAL,
+	SCPI_CMD_SET_ACVOLTAGE,
+	SCPI_CMD_SET_ACDCVOLTAGE,
+	SCPI_CMD_SET_DCVOLTAGE,
+	SCPI_CMD_SET_ACCURRENT,
+	SCPI_CMD_SET_ACDCCURRENT,
+	SCPI_CMD_SET_DCCURRENT,
+	SCPI_CMD_SET_FREQUENCY,
+	SCPI_CMD_SET_RESISTANCE,
+	SCPI_CMD_SET_CONTINUITY,
+	SCPI_CMD_SET_DIODE,
+	SCPI_CMD_SET_AUTO,
+	SCPI_CMD_GET_AUTO,
+	SCPI_CMD_SET_FIXED,
+	SCPI_CMD_SET_RANGE,
+	SCPI_CMD_GET_RANGE_D1,
+	SCPI_CMD_GET_RANGE_D2,
+	SCPI_CMD_SET_DB,
+	SCPI_CMD_SET_DBCLR,
+	SCPI_CMD_SET_DBPOWER,
+	SCPI_CMD_SET_DBREF,
+	SCPI_CMD_GET_DBREF,
+	SCPI_CMD_SET_HOLD,
+	SCPI_CMD_SET_HOLDCLR,
+	SCPI_CMD_SET_MAX,
+	SCPI_CMD_SET_MIN,
+	SCPI_CMD_SET_MMCLR,
+	SCPI_CMD_SET_REL,
+	SCPI_CMD_SET_RELCLR,
+	SCPI_CMD_GET_MEAS_DD,
+	SCPI_CMD_GET_MEAS_D1,
+	SCPI_CMD_GET_MEAS_D2,
+	SCPI_CMD_GET_RATE,
+	SCPI_CMD_SET_RATE,
+	SCPI_CMD_SET_TRIGGER,
+	SCPI_CMD_GET_TRIGGER,
+};
+
+static const struct scpi_command fluke_45_cmdset[] = {
+	{ SCPI_CMD_CLS, "*CLS" },
+	{ SCPI_CMD_RST, "*RST" },
+	{ SCPI_CMD_REMS, "*REMS" },
+	{ SCPI_CMD_RWLS, "*RWLS" },
+	{ SCPI_CMD_LOCS, "LOCS" },
+	{ SCPI_CMD_LWLS, "LWLS" },
+	{ SCPI_CMD_REMOTE, "REMS" },
+	{ SCPI_CMD_LOCAL, "LOCS" },
+	{ SCPI_CMD_SET_ACVOLTAGE, "VAC" },
+	{ SCPI_CMD_SET_ACDCVOLTAGE, "VACDC" },
+	{ SCPI_CMD_SET_DCVOLTAGE, "VDC" },
+	{ SCPI_CMD_SET_ACCURRENT, "AAC" },
+	{ SCPI_CMD_SET_ACDCCURRENT, "AACDC" },
+	{ SCPI_CMD_SET_DCCURRENT, "ADC" },
+	{ SCPI_CMD_SET_FREQUENCY, "FREQ" },
+	{ SCPI_CMD_SET_RESISTANCE, "OHMS" },
+	{ SCPI_CMD_SET_CONTINUITY, "CONT" },
+	{ SCPI_CMD_SET_DIODE, "DIODE" },
+	{ SCPI_CMD_SET_AUTO, "AUTO" },
+	{ SCPI_CMD_GET_AUTO, "AUTO?" },
+	{ SCPI_CMD_SET_FIXED, "FIXED" },
+	{ SCPI_CMD_SET_RANGE, "RANGE" },
+	{ SCPI_CMD_GET_RANGE_D1, "RANGE1?" },
+	{ SCPI_CMD_GET_RANGE_D2, "RANGE2?" },
+	{ SCPI_CMD_SET_DB, "DB" },
+	{ SCPI_CMD_SET_DBCLR, "DBCLR" },
+	{ SCPI_CMD_SET_DBPOWER, "DBPOWER" },
+	{ SCPI_CMD_SET_DBREF, "DBREF" },
+	{ SCPI_CMD_GET_DBREF, "DBREF?" },
+	{ SCPI_CMD_SET_HOLD, "HOLD" },
+	{ SCPI_CMD_SET_HOLDCLR, "HOLDCLR" },
+	{ SCPI_CMD_SET_MAX, "MAX" },
+	{ SCPI_CMD_SET_MIN, "MIN" },
+	{ SCPI_CMD_SET_MMCLR, "MMCLR" },
+	{ SCPI_CMD_SET_REL, "REL" },
+	{ SCPI_CMD_SET_RELCLR, "RELCLR" },
+	{ SCPI_CMD_GET_MEAS_DD, "MEAS?" },
+	{ SCPI_CMD_GET_MEAS_D1, "MEAS1?" },
+	{ SCPI_CMD_GET_MEAS_D2, "MEAS2?" },
+	{ SCPI_CMD_SET_RATE, "RATE" },
+	{ SCPI_CMD_GET_RATE, "RATE?" },
+	{ SCPI_CMD_SET_TRIGGER, "TRIGGER" },
+	{ SCPI_CMD_GET_TRIGGER, "TRIGGER?" },
+	ALL_ZERO
+};
+
+struct fluke_scpi_dmm_model {
+	const char *vendor;
+	const char *model;
+	int num_channels;
+	int poll_period; /* How often to poll, in ms. */
+};
+
+struct channel_spec {
+	const char *name;
+	/* Min, max, programming resolution, spec digits, encoding digits. */
+	double voltage[5];
+	double current[5];
+	double resistance[5];
+	double capacitance[5];
+	double conductance[5];
+	double diode[5];
+};
+
+struct channel_group_spec {
+	const char *name;
+	uint64_t channel_index_mask;
+	uint64_t features;
+};
+
+struct dmm_channel {
+	enum sr_mq mq;
+	unsigned int hw_output_idx;
+	const char *hwname;
+	int digits;
+};
+
+struct dmm_channel_instance {
+	enum sr_mq mq;
+	int command;
+	const char *prefix;
+};
+
+struct dmm_channel_group {
+	uint64_t features;
+};
+
+struct dev_context {
+	struct sr_sw_limits limits;
+	unsigned int num_channels;
+	const struct scpi_command *cmdset;
+	char *response;
+	const char *mode1;
+	const char *mode2;
+	long range1;
+	long range2;
+	long autorng;
+	const char *rate;
+	long modifiers;
+	long trigmode;
+};
+
+int get_reading_dd(char *reading, size_t size);
+
+SR_PRIV extern const struct fluke_scpi_dmm_model dmm_profiles[];
+
+SR_PRIV int fl45_scpi_receive_data(int fd, int revents, void *cb_data);
+SR_PRIV int fl45_scpi_get_response(const struct sr_dev_inst *sdi,  char *cmd);
+SR_PRIV int fl45_get_status(const struct sr_dev_inst *sdi,
+		struct sr_datafeed_analog *analog, int idx);
+SR_PRIV int fl45_get_modifiers(const struct sr_dev_inst *sdi,
+		struct sr_datafeed_analog *analog, int idx);
 
 #endif
