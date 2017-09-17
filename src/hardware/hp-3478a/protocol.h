1d9eebf4bef4f74f2f2f0f0f9c29836f8d5369e6
Frank Stettner <frank-stettner@gmx.net>
2017-09-17 15:04:06 +0200

    hp-3478a: Initial driver skeleton.

---
+++ src/hardware/hp-3478a/protocol.h
@@ -0,0 +1,35 @@
+/*
+ * This file is part of the libsigrok project.
+ *
+ * Copyright (C) 2017 Frank Stettner <frank-stettner@gmx.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LIBSIGROK_HARDWARE_HP_3478A_PROTOCOL_H
+#define LIBSIGROK_HARDWARE_HP_3478A_PROTOCOL_H
+
+#include <stdint.h>
+#include <glib.h>
+#include <libsigrok/libsigrok.h>
+#include "libsigrok-internal.h"
+
+#define LOG_PREFIX "hp-3478a"
+
+struct dev_context {
+};
+
+SR_PRIV int hp_3478a_receive_data(int fd, int revents, void *cb_data);
+
+#endif
