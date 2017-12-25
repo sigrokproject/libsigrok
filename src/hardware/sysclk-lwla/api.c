sigrokproject/libsigrok

commit f1ba6b4b2c9a8ecf90bb31efb218752aa7e49d1a

sr_dev_close(): Set status to SR_ST_INACTIVE.
This ensures consistent checks and log messages across all drivers
and reduces the per-driver boilerplate.

src/hardware/sysclk-lwla/api.c
@@ -324,8 +324,7 @@ static int dev_open(struct sr_dev_inst *sdi)
 	return ret;
 }
 
-/* Shutdown and close device.
- */
+/* Shutdown and close device. */
 static int dev_close(struct sr_dev_inst *sdi)
 {
 	struct dev_context *devc;
@@ -342,17 +341,16 @@ static int dev_close(struct sr_dev_inst *sdi)
 		return SR_ERR_BUG;
 	}
 
-	sdi->status = SR_ST_INACTIVE;
-
 	/* Download of the shutdown bitstream, if any. */
 	ret = (*devc->model->apply_fpga_config)(sdi);
 	if (ret != SR_OK)
 		sr_warn("Unable to shut down device.");
 
 	libusb_release_interface(usb->devhdl, USB_INTERFACE);
+
 	sr_usb_close(usb);
 
-	return ret;
+	return SR_OK;
 }
 
 /* Check whether the device options contain a specific key.

src/hardware/asix-sigma/api.c
@@ -156,11 +156,7 @@ static int dev_close(struct sr_dev_inst *sdi)
 
 	devc = sdi->priv;
 
-	ftdi_usb_close(&devc->ftdic);
-
-	sdi->status = SR_ST_INACTIVE;
-
-	return SR_OK;
+	return (ftdi_usb_close(&devc->ftdic) == 0) ? SR_OK : SR_ERR;
 }
 
 static int config_get(uint32_t key, GVariant **data, const struct sr_dev_inst *sdi,

src/hardware/atten-pps3xxx/api.c
@@ -421,6 +421,7 @@ static int dev_close(struct sr_dev_inst *sdi)
 	struct dev_context *devc;
 
 	devc = sdi->priv;
+
 	if (devc->config_dirty)
 		/* Some configuration changes were queued up but didn't
 		 * get sent to the device, likely because we were never

src/hardware/baylibre-acme/api.c
@@ -137,8 +137,6 @@ static int dev_close(struct sr_dev_inst *sdi)
 {
 	(void)sdi;
 
-	sdi->status = SR_ST_INACTIVE;
-
 	return SR_OK;
 }
 

src/hardware/beaglelogic/api.c
@@ -168,8 +168,6 @@ static int dev_close(struct sr_dev_inst *sdi)
 	beaglelogic_munmap(devc);
 	beaglelogic_close(devc);
 
-	sdi->status = SR_ST_INACTIVE;
-
 	return SR_OK;
 }
 

src/hardware/brymen-bm86x/api.c
@@ -134,24 +134,18 @@ static int dev_close(struct sr_dev_inst *sdi)
 
 	if ((ret = libusb_release_interface(usb->devhdl, 0)))
 		sr_err("Failed to release interface 0: %s.\n", libusb_error_name(ret));
-	else
-		sr_dbg("Successfully released interface 0.\n");
 
 	if (!ret && devc->detached_kernel_driver) {
-		if ((ret = libusb_attach_kernel_driver(usb->devhdl, 0))) {
+		if ((ret = libusb_attach_kernel_driver(usb->devhdl, 0)))
 			sr_err("Failed to attach kernel driver: %s.\n",
 			       libusb_error_name(ret));
-		} else {
+		else
 			devc->detached_kernel_driver = 0;
-			sr_dbg("Successfully attached kernel driver.\n");
-		}
 	}
 
 	libusb_close(usb->devhdl);
 
-	sdi->status = SR_ST_INACTIVE;
-
-	return ret;
+	return (ret == 0) ? SR_OK : SR_ERR;
 }
 
 static int config_get(uint32_t key, GVariant **data, const struct sr_dev_inst *sdi,

src/hardware/chronovu-la/api.c
@@ -287,13 +287,14 @@ static int dev_close(struct sr_dev_inst *sdi)
 
 	devc = sdi->priv;
 
-	if (devc->ftdic && (ret = ftdi_usb_close(devc->ftdic)) < 0)
+	if (!devc->ftdic)
+		return SR_ERR_BUG;
+
+	if ((ret = ftdi_usb_close(devc->ftdic)) < 0)
 		sr_err("Failed to close FTDI device (%d): %s.",
 		       ret, ftdi_get_error_string(devc->ftdic));
 
-	sdi->status = SR_ST_INACTIVE;
-
-	return SR_OK;
+	return (ret == 0) ? SR_OK : SR_ERR;
 }
 
 static int config_get(uint32_t key, GVariant **data, const struct sr_dev_inst *sdi,

src/hardware/demo/api.c
@@ -189,7 +189,7 @@ static int dev_open(struct sr_dev_inst *sdi)
 
 static int dev_close(struct sr_dev_inst *sdi)
 {
-	sdi->status = SR_ST_INACTIVE;
+	(void)sdi;
 
 	return SR_OK;
 }

commit 42d14d9108de61ead41aebe74d668239ee7d5089

sysclk-lwla: Fix a regression with (at least) the LWLA1034.
This regression was introduced in f1ba6b4b2c9a8ecf90bb31efb218752aa7e49d1a.

Due to how the sysclk-lwla driver does up to 3 open/close operations in
one dev_open() API callback we cannot rely on the sr_dev_open() and
sr_dev_close() wrappers setting the sdi->status variable in this case.

Tested on LWLA1034.

src/hardware/sysclk-lwla/api.c
@@ -259,6 +259,8 @@ static int dev_open(struct sr_dev_inst *sdi)
 		/* This delay appears to be necessary for reliable operation. */
 		g_usleep(30 * 1000);
 
+		sdi->status = SR_ST_ACTIVE;
+
 		devc->active_fpga_config = FPGA_NOCONF;
 		devc->short_transfer_quirk = FALSE;
 		devc->state = STATE_IDLE;
@@ -271,6 +273,7 @@ static int dev_open(struct sr_dev_inst *sdi)
 			break;
 
 		/* Rinse and repeat. */
+		sdi->status = SR_ST_INACTIVE;
 		sr_usb_close(usb);
 	}
 
@@ -306,7 +309,7 @@ static int dev_close(struct sr_dev_inst *sdi)
 
 	sr_usb_close(usb);
 
-	return SR_OK;
+	return ret;
 }
 
 /* Check whether the device options contain a specific key.