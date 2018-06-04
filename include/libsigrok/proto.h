b736fa0c7d3505c9902fa5c2250e6fdf2b74f560
Gerhard Sittig <gerhard.sittig@gmx.net>
2018-06-04 23:44:30 +0200

    proto.h: move routine declaration to its respective group

--- include/libsigrok/proto.h
+++ include/libsigrok/proto.h
@@ -164,8 +164,6 @@ SR_API const char *const *sr_input_extensions_get(
 		const struct sr_input_module *imod);
 SR_API const struct sr_input_module *sr_input_find(char *id);
 SR_API const struct sr_option **sr_input_options_get(const struct sr_input_module *imod);
-SR_API gboolean sr_output_test_flag(const struct sr_output_module *omod,
-		uint64_t flag);
 SR_API void sr_input_options_free(const struct sr_option **options);
 SR_API struct sr_input *sr_input_new(const struct sr_input_module *imod,
 		GHashTable *options);
@@ -192,6 +190,8 @@ SR_API void sr_output_options_free(const struct sr_option **opts);
 SR_API const struct sr_output *sr_output_new(const struct sr_output_module *omod,
 		GHashTable *params, const struct sr_dev_inst *sdi,
 		const char *filename);
+SR_API gboolean sr_output_test_flag(const struct sr_output_module *omod,
+		uint64_t flag);
 SR_API int sr_output_send(const struct sr_output *o,
 		const struct sr_datafeed_packet *packet, GString **out);
 SR_API int sr_output_free(const struct sr_output *o);
