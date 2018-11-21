090bcdf240a5dadd17ceb751d372684ecd4ad77d
Guido Trentalancia <guido@trentalancia.com>
2018-11-22 00:27:33 +0100

    Prefer the unsigned int type over the int type for the options array in the "rohde-schwarz-hameg" driver.
    Signed-off-by: Guido Trentalancia <guido@trentalancia.com>
    ---
     src/hardware/rohde-schwarz-hameg/protocol.c |    2 +-
     src/hardware/rohde-schwarz-hameg/protocol.h |   10 +++++-----
     2 files changed, 6 insertions(+), 6 deletions(-)

--- src/hardware/rohde-schwarz-hameg/protocol.h
+++ src/hardware/rohde-schwarz-hameg/protocol.h
@@ -84,7 +84,7 @@ struct scope_config {
 };
 
 struct analog_channel_state {
-	int coupling;
+	unsigned int coupling;
 
 	int vscale;
 	float vertical_offset;
@@ -96,7 +96,7 @@ struct analog_channel_state {
 struct digital_pod_state {
 	gboolean state;
 
-	int threshold;
+	unsigned int threshold;
 	float user_threshold;
 };
 
@@ -108,12 +108,12 @@ struct scope_state {
 	int timebase;
 
 	uint64_t sample_rate;
-	int waveform_sample_rate;
+	unsigned int waveform_sample_rate;
 
 	float horiz_triggerpos;
 
-	int trigger_source;
-	int trigger_slope;
+	unsigned int trigger_source;
+	unsigned int trigger_slope;
 	char trigger_pattern[MAX_ANALOG_CHANNEL_COUNT + MAX_DIGITAL_CHANNEL_COUNT];
 
 	gboolean high_resolution;
