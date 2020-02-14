67a92fc43cecf517e64f383e0076311e4752f310
Jan Metzger <jm@metweb.de>
2020-02-14 08:38:27 +0100

    Enable Edge-Triggering capabilities on the Zeroplus Logic Analyzer driver.

--- src/hardware/zeroplus-logic-cube/api.c
+++ src/hardware/zeroplus-logic-cube/api.c
@@ -71,6 +71,9 @@ static const uint32_t devopts[] = {
 static const int32_t trigger_matches[] = {
 	SR_TRIGGER_ZERO,
 	SR_TRIGGER_ONE,
+	SR_TRIGGER_RISING,
+	SR_TRIGGER_FALLING,
+	SR_TRIGGER_EDGE
 };
 
 /*
