sigrokproject/libsigrok

commit cb828f1b3e00ccd0ad23d7cb98dca4d0d4749b07

serial: flush() after open() in the serial core.
Quite some drivers flush the serial port after opening it. And quite
some don't although they should. Factor this out, so serial_open() will
always flush the port. The removal in the drivers was done with this
small coccinelle script:

@@
struct sr_serial_dev_inst *serial;
@@

        serial_open(serial, ...)
        ... when != serial
-       serial_flush(serial);

and then the results and the unmatched findings of serial_flush() were
audited.

Signed-off-by: Wolfram Sang <wsa@kernel.org>

src/serial.c
@@ -122,10 +122,13 @@ SR_PRIV int serial_open(struct sr_serial_dev_inst *serial, int flags)
 	if (ret != SR_OK)
 		return ret;
 
-	if (serial->serialcomm)
-		return serial_set_paramstr(serial, serial->serialcomm);
-	else
-		return SR_OK;
+	if (serial->serialcomm) {
+		ret = serial_set_paramstr(serial, serial->serialcomm);
+		if (ret != SR_OK)
+			return ret;
+	}
+
+	return serial_flush(serial);
 }
 
 /**

src/hardware/agilent-dmm/api.c
@@ -131,7 +131,6 @@ static GSList *scan(struct sr_dev_driver *di, GSList *options)
 	if (serial_open(serial, SERIAL_RDWR) != SR_OK)
 		return NULL;
 
-	serial_flush(serial);
 	if (serial_write_blocking(serial, "*IDN?\r\n", 7, SERIAL_WRITE_TIMEOUT_MS) < 7) {
 		sr_err("Unable to send identification string.");
 		return NULL;

src/hardware/appa-55ii/api.c
@@ -79,8 +79,6 @@ static GSList *scan(struct sr_dev_driver *di, GSList *options)
 
 	sr_info("Probing serial port %s.", conn);
 
-	serial_flush(serial);
-
 	/* Let's get a bit of data and see if we can find a packet. */
 	if (serial_stream_detect(serial, buf, &len, 25,
 			appa_55ii_packet_valid, 500) != SR_OK)

src/hardware/arachnid-labs-re-load-pro/api.c
@@ -99,8 +99,6 @@ static GSList *scan(struct sr_dev_driver *di, GSList *options)
 	if (serial_open(serial, SERIAL_RDWR) != SR_OK)
 		return NULL;
 
-	serial_flush(serial);
-
 	/*
 	 * First stop potentially running monitoring and wait for 50ms before
 	 * next command can be sent.

src/hardware/atten-pps3xxx/api.c
@@ -113,8 +113,6 @@ static GSList *scan(struct sr_dev_driver *di, GSList *options, int modelid)
 	if (serial_open(serial, SERIAL_RDWR) != SR_OK)
 		return NULL;
 
-	serial_flush(serial);
-
 	/* This is how the vendor software scans for hardware. */
 	memset(packet, 0, PACKET_SIZE);
 	packet[0] = 0xaa;

src/hardware/center-3xx/api.c
@@ -67,8 +67,6 @@ static GSList *center_scan(const char *conn, const char *serialcomm, int idx)
 	if (serial_open(serial, SERIAL_RDWR) != SR_OK)
 		return NULL;
 
-	serial_flush(serial);
-
 	sr_info("Found device on port %s.", conn);
 
 	sdi = g_malloc0(sizeof(struct sr_dev_inst));

src/hardware/conrad-digi-35-cpu/api.c
@@ -75,7 +75,6 @@ static GSList *scan(struct sr_dev_driver *di, GSList *options)
 	if (serial_open(serial, SERIAL_RDWR) != SR_OK)
 		return NULL;
 
-	serial_flush(serial);
 	serial_close(serial);
 
 	sr_spew("Conrad DIGI 35 CPU assumed at %s.", conn);

src/hardware/gmc-mh-1x-2x/api.c
@@ -102,6 +102,7 @@ static enum model scan_model_sm(struct sr_serial_dev_inst *serial)
 	 * Try to find message consisting of device code and several
 	 * (at least 4) data bytes.
 	 */
+	serial_flush(serial);
 	for (bytecnt = 0; bytecnt < 100; bytecnt++) {
 		byte = read_byte(serial, timeout_us);
 		if ((byte == -1) || (timeout_us < g_get_monotonic_time()))
@@ -175,8 +176,6 @@ static GSList *scan_1x_2x_rs232(struct sr_dev_driver *di, GSList *options)
 		return NULL;
 	}
 
-	serial_flush(serial);
-
 	model = scan_model_sm(serial);
 
 	/*
@@ -187,10 +186,8 @@ static GSList *scan_1x_2x_rs232(struct sr_dev_driver *di, GSList *options)
 		serialcomm = SERIALCOMM_1X_RS232;
 		g_free(serial->serialcomm);
 		serial->serialcomm = g_strdup(serialcomm);
-		if (serial_set_paramstr(serial, serialcomm) == SR_OK) {
-			serial_flush(serial);
+		if (serial_set_paramstr(serial, serialcomm) == SR_OK)
 			model = scan_model_sm(serial);
-		}
 	}
 
 	if (model != METRAHIT_NONE) {

commit 0dc27cd1dbc961366d14607526e54992078e9d5c

serial: make failed flush() in open() non-fatal, CP2110 flush() return
Commit cb828f1b3e00 introduced an unconditional flush() call in the
open() routine's body, and passed its return value in verbatim form to
open() callers. Some of the transports/cables for serial communication
yield the SR_ERR_NA return value. Consider this a non-fatal condition.

Unbreak the CP2110 HID chip's flush() implementation. Don't expect a
return value of 0 from HID write calls, instead expect to see the number
of written bytes for successful calls.

This was tested with ch9325 (UT-D04), cp2110 (UT-D09) and bu86x.

src/serial.c
@@ -128,7 +128,18 @@ SR_PRIV int serial_open(struct sr_serial_dev_inst *serial, int flags)
 			return ret;
 	}
 
-	return serial_flush(serial);
+	/*
+	 * Flush potentially dangling RX data. Availability of the
+	 * flush primitive depends on the transport/cable, absense
+	 * is non-fatal.
+	 */
+	ret = serial_flush(serial);
+	if (ret == SR_ERR_NA)
+		ret = SR_OK;
+	if (ret != SR_OK)
+		return ret;
+
+	return SR_OK;
 }
 
 /**

src/serial_hid_cp2110.c
@@ -280,7 +280,7 @@ static int cp2110_flush(struct sr_serial_dev_inst *serial)
 	buffer[0] = CP2110_FIFO_PURGE;
 	buffer[1] = CP2110_FIFO_PURGE_TX | CP2110_FIFO_PURGE_RX;
 	rc = ser_hid_hidapi_set_data(serial, 0, buffer, sizeof(buffer), 0);
-	if (rc != 0)
+	if (rc != sizeof(buffer))
 		return SR_ERR;
 	return SR_OK;
 }