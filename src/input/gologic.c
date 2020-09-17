46da90676800456ed5b588495707e00afec09710
xorloser <me@xorloser.com>
2020-09-17 22:18:08 +1000

    Added sigrok license header.

--- src/input/gologic.c
+++ src/input/gologic.c
@@ -1,7 +1,7 @@
 /*
  * This file is part of the libsigrok project.
  *
- * Copyright (C) 2020
+ * Copyright (C) 2020 xorloser me@xorloser.com
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
