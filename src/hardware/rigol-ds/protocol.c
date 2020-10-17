bc17eb727b943fca8e78889c79cd59beadeba978
Ralf <jr-oss@gmx.net>
2020-10-17 18:51:25 +0200

    rigol-ds: Let all scpi commands start with ':'

--- src/hardware/rigol-ds/protocol.c
+++ src/hardware/rigol-ds/protocol.c
@@ -448,7 +448,7 @@ SR_PRIV int rigol_ds_capture_start(const struct sr_dev_inst *sdi)
 			rigol_ds_set_wait_event(devc, WAIT_STOP);
 			if (devc->data_source == DATA_SOURCE_SEGMENTED &&
 					devc->model->series->protocol <= PROTOCOL_V4)
-				if (rigol_ds_config_set(sdi, "FUNC:WREP:FCUR %d", devc->num_frames + 1) != SR_OK)
+				if (rigol_ds_config_set(sdi, ":FUNC:WREP:FCUR %d", devc->num_frames + 1) != SR_OK)
 					return SR_ERR;
 		}
 		break;
@@ -950,9 +950,9 @@ SR_PRIV int rigol_ds_receive(int fd, int revents, void *cb_data)
 		if (devc->data_source == DATA_SOURCE_SEGMENTED &&
 				devc->model->series->protocol == PROTOCOL_V5) {
 			int frames = 0;
-			if (rigol_ds_config_set(sdi, "REC:CURR %d", devc->num_frames + 1) != SR_OK)
+			if (rigol_ds_config_set(sdi, ":REC:CURR %d", devc->num_frames + 1) != SR_OK)
 				return SR_ERR;
-			if (sr_scpi_get_int(sdi->conn, "REC:CURR?", &frames) != SR_OK)
+			if (sr_scpi_get_int(sdi->conn, ":REC:CURR?", &frames) != SR_OK)
 				return SR_ERR;
 			devc->num_frames_segmented = frames;
 		}
