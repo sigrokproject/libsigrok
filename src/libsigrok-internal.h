c5706c4a33d6ee432140e48d9730e865e9c51d1f
Cymaphore <x@cymaphore.net>
2020-11-02 14:15:51 +0100

    Commentary and TODOs

--- src/libsigrok-internal.h
+++ src/libsigrok-internal.h
@@ -2608,16 +2608,19 @@ SR_PRIV int sr_eev121gw_3displays_parse(const uint8_t *buf, float *floatval,
 /**
  * Model 150: 3 readings/s
  * @TODO Get hands on a CM 12 or other 150 and check if that actually is the case
+ * @TODO fix delay to meet 10 Hz rate of device
  */
 #define APPA_B_DELAY_MODEL_150 333
 
 /**
  * Model 208: 10 readings/s
+ * @TODO fix delay to meet 10 Hz rate of device
  */
 #define APPA_B_DELAY_MODEL_208 100
 
 /**
  * Model 506: 10 readings/s
+ * @TODO fix delay to meet 10 Hz rate of device
  */
 #define APPA_B_DELAY_MODEL_506 100
 
