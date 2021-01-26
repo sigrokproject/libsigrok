42de6b4284451ec6849ce371f6094c87627b6238
Peter Skarpetis <peters@skarpetis.com>
2021-01-26 11:48:32 +1100

    mhinstek-mhs-5200a: Initial MHINSTEK MHS-5200A driver implementation
    This implements support for MHINSTEK MHS-5200A series of function
    generators. Driver provides basic control via channel groups
    ("1", and "2"). Acquisition returns data from the built-in
    frequency counter.
    
    Supported models: MHS-5200-06M, MHS-5200-12M, MHS-5200-20M,
    MHS-5200-25M

--- src/hardware/mhinstek-mhs-5200a/api.c
+++ src/hardware/mhinstek-mhs-5200a/api.c
@@ -17,123 +17,401 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <math.h>
+
 #include <config.h>
 #include "protocol.h"
 
-static struct sr_dev_driver mhinstek_mhs_5200a_driver_info;
+static struct sr_dev_driver mhinstek_mhs5200a_driver_info;
+
+static const uint32_t scanopts[] = {
+	SR_CONF_CONN,
+};
+
+static const uint32_t drvopts[] = {
+	SR_CONF_SIGNAL_GENERATOR,
+};
+
+static const uint32_t mhs5200a_devopts[] = {
+	SR_CONF_CONTINUOUS,
+	SR_CONF_LIMIT_SAMPLES | SR_CONF_GET | SR_CONF_SET,
+	SR_CONF_LIMIT_MSEC | SR_CONF_GET | SR_CONF_SET,
+};
+
+static const uint32_t mhs5200a_devopts_cg[] = {
+	SR_CONF_ENABLED | SR_CONF_GET | SR_CONF_SET,
+	SR_CONF_PATTERN_MODE | SR_CONF_GET | SR_CONF_SET | SR_CONF_LIST,
+	SR_CONF_OUTPUT_FREQUENCY | SR_CONF_GET | SR_CONF_SET | SR_CONF_LIST,
+	SR_CONF_AMPLITUDE | SR_CONF_GET | SR_CONF_SET,
+	SR_CONF_OFFSET | SR_CONF_GET | SR_CONF_SET,
+	SR_CONF_PHASE | SR_CONF_GET | SR_CONF_SET | SR_CONF_LIST,
+	SR_CONF_DUTY_CYCLE | SR_CONF_GET | SR_CONF_SET,
+};
+
+#define WAVEFORM_DEFAULT WFO_FREQUENCY | WFO_AMPLITUDE | WFO_OFFSET | WFO_PHASE
+
+static const struct waveform_spec mhs5200a_waveforms[] = {
+	{ WAVEFORM_SINE,             1.0E-6,  21.0E+6, 1.0E-6, WAVEFORM_DEFAULT },
+	{ WAVEFORM_SQUARE,           1.0E-6,   6.0E+6, 1.0E-6, WAVEFORM_DEFAULT | WFO_DUTY_CYCLE },
+	{ WAVEFORM_TRIANGLE,         1.0E-6,   6.0E+6, 1.0E-6, WAVEFORM_DEFAULT },
+	{ WAVEFORM_RISING_SAWTOOTH,  1.0E-6,   6.0E+6, 1.0E-6, WAVEFORM_DEFAULT },
+	{ WAVEFORM_FALLING_SAWTOOTH, 1.0E-6,   6.0E+6, 1.0E-6, WAVEFORM_DEFAULT },
+};
+
+static const struct channel_spec mhs5200a_channels[] = {
+	{ "CH1",  ARRAY_AND_SIZE(mhs5200a_waveforms) },
+	{ "CH2",  ARRAY_AND_SIZE(mhs5200a_waveforms) },
+};
+
+static const double phase_min_max_step[] = { 0.0, 360.0, 1.0 };
+
+SR_PRIV int mhs5200a_frequency_limits(enum waveform_type wtype, double *freq_min, double *freq_max)
+{
+	const struct waveform_spec *wspec;
+	unsigned int i;
+	
+	wspec = NULL;
+	for (i = 0; i < ARRAY_SIZE(mhs5200a_waveforms); i++) {
+		if (mhs5200a_waveforms[i].waveform == wtype) {
+			wspec = &mhs5200a_waveforms[i];
+			break;
+		}
+	}
+	if (!wspec) {
+		sr_err("Could not determine current pattern type");
+		return SR_ERR;
+	}
+	*freq_min = wspec->freq_min;
+	*freq_max = wspec->freq_max;
+	return SR_OK;
+}
 
 static GSList *scan(struct sr_dev_driver *di, GSList *options)
 {
-	struct drv_context *drvc;
-	GSList *devices;
-
-	(void)options;
+	struct dev_context *devc;
+	struct sr_serial_dev_inst *serial;
+	struct sr_config *src;
+	const char *conn, *serialcomm;
+	struct sr_dev_inst *sdi;
+	struct sr_channel *ch;
+	struct sr_channel_group *cg;
+	GSList *l, *devices;
+	unsigned int i, ch_idx;
+	char buf[PROTOCOL_LEN_MAX];
 
 	devices = NULL;
-	drvc = di->context;
-	drvc->instances = NULL;
+	conn = NULL;
+	serialcomm = "57600/8n1";
+	for (l = options; l; l = l->next) {
+		src = l->data;
+		switch (src->key) {
+		case SR_CONF_CONN:
+			conn = g_variant_get_string(src->data, NULL);
+			break;
+		case SR_CONF_SERIALCOMM:
+			serialcomm = g_variant_get_string(src->data, NULL);
+			break;
+		}
+	}
+	if (!conn)
+		return NULL;
+	
+	serial = sr_serial_dev_inst_new(conn, serialcomm);
 
-	/* TODO: scan for devices, either based on a SR_CONF_CONN option
-	 * or on a USB scan. */
+	if (serial_open(serial, SERIAL_RDWR) != SR_OK)
+		return NULL;
 
-	return devices;
-}
+	sr_info("Probing serial port %s.", conn);
 
-static int dev_open(struct sr_dev_inst *sdi)
-{
-	(void)sdi;
+	
+	/* Query and verify model string. */
+	if (mhs5200a_get_model(serial, buf, sizeof(buf)) < 0) {
+		serial_close(serial);
+		return NULL;
+	}
+	sr_info("Found device on port %s.", conn);
 
-	/* TODO: get handle from sdi->conn and open it. */
+	sdi = g_malloc0(sizeof(*sdi));
+	sdi->status = SR_ST_INACTIVE;
+	sdi->vendor = g_strdup("MHINSTEK");
+	sdi->model = g_strdup(buf);
+	//sdi->version = g_strdup("5.04");
+	//sdi->serial_num = g_strdup("1234");
+	sdi->driver = &mhinstek_mhs5200a_driver_info;
 
-	return SR_OK;
-}
+	
+	devc = g_malloc0(sizeof(*devc));
+	sr_sw_limits_init(&devc->limits);
+	devc->max_frequency = (buf[6] - '0') * 10 + buf[7] - '0';
+	devc->max_frequency *= 1.0e06;
+	sdi->inst_type = SR_INST_SERIAL;
+	sdi->conn = serial;
+	sdi->priv = devc;
 
-static int dev_close(struct sr_dev_inst *sdi)
-{
-	(void)sdi;
+	/* Create channel group and channel for each device channel. */
+	ch_idx = 0;
+	for (i = 0; i < ARRAY_SIZE(mhs5200a_channels); i++) {
+		ch = sr_channel_new(sdi, ch_idx++, SR_CHANNEL_ANALOG, TRUE,
+				mhs5200a_channels[i].name);
+		cg = g_malloc0(sizeof(*cg));
+		snprintf(buf, sizeof(buf), "%u", i + 1);
+		cg->name = g_strdup(buf);
+		cg->channels = g_slist_append(cg->channels, ch);
 
-	/* TODO: get handle from sdi->conn and close it. */
+		sdi->channel_groups = g_slist_append(sdi->channel_groups, cg);
+	}
+	
+	/* Create channels for the frequency counter output. */
+	ch = sr_channel_new(sdi, ch_idx++, SR_CHANNEL_ANALOG, TRUE, "FREQ");
+	ch = sr_channel_new(sdi, ch_idx++, SR_CHANNEL_ANALOG, TRUE, "PERIOD");
+	ch = sr_channel_new(sdi, ch_idx++, SR_CHANNEL_ANALOG, TRUE, "DUTY");
+	ch = sr_channel_new(sdi, ch_idx++, SR_CHANNEL_ANALOG, TRUE, "WIDTH");
 
-	return SR_OK;
+	/* Add found device to result set. */
+	devices = g_slist_append(devices, sdi);
+
+	serial_close(serial);
+
+	return std_scan_complete(di, devices);
 }
 
 static int config_get(uint32_t key, GVariant **data,
 	const struct sr_dev_inst *sdi, const struct sr_channel_group *cg)
 {
+	struct dev_context *devc;
+	struct sr_channel *ch;
+	const struct sr_key_info *kinfo;
+	double valf;
+	long vall;
 	int ret;
+	
+	if (!sdi || !data)
+		return SR_ERR_ARG;
 
-	(void)sdi;
-	(void)data;
-	(void)cg;
-
+	devc = sdi->priv;
 	ret = SR_OK;
-	switch (key) {
-	/* TODO */
-	default:
-		return SR_ERR_NA;
-	}
+	kinfo = sr_key_info_get(SR_KEY_CONFIG, key);
 
+	if (!cg) {
+		switch (key) {
+		case SR_CONF_LIMIT_SAMPLES:
+		case SR_CONF_LIMIT_MSEC:
+			ret = sr_sw_limits_config_get(&devc->limits, key, data);
+			break;
+		default:
+			sr_dbg("%s: Unsupported key: %d (%s)", __func__,
+				(int)key, (kinfo ? kinfo->name : "unknown"));
+			ret = SR_ERR_NA;
+			break;
+		}
+	} else {
+		ch = cg->channels->data;
+
+		switch (key) {
+		case SR_CONF_ENABLED:
+			if ((ret = mhs5200a_get_onoff(sdi, &vall)) == SR_OK) {
+				*data = g_variant_new_boolean(vall);
+			}
+			break;
+		case SR_CONF_PATTERN_MODE:
+			if ((ret = mhs5200a_get_waveform(sdi, ch->index + 1, &vall)) == SR_OK) {
+				*data = g_variant_new_string(mhs5200a_waveform_to_string(vall));
+			}
+			break;
+		case SR_CONF_OUTPUT_FREQUENCY:
+			if ((ret = mhs5200a_get_frequency(sdi, ch->index + 1, &valf)) == SR_OK)
+				*data = g_variant_new_double(valf);
+			break;
+		case SR_CONF_AMPLITUDE:
+			if ((ret = mhs5200a_get_amplitude(sdi, ch->index + 1, &valf)) == SR_OK)
+				*data = g_variant_new_double(valf);
+			break;
+		case SR_CONF_OFFSET:
+			if ((ret = mhs5200a_get_offset(sdi, ch->index + 1, &valf)) == SR_OK)
+				*data = g_variant_new_double(valf);
+			break;
+		case SR_CONF_PHASE:
+			if ((ret = mhs5200a_get_phase(sdi, ch->index + 1, &valf)) == SR_OK)
+				*data = g_variant_new_double(valf);
+			break;
+		case SR_CONF_DUTY_CYCLE:
+			if ((ret = mhs5200a_get_duty_cycle(sdi, ch->index + 1, &valf)) == SR_OK)
+				*data = g_variant_new_double(valf);
+			break;
+		default:
+			sr_dbg("%s: Unsupported (cg) key: %d (%s)", __func__,
+				(int)key, (kinfo ? kinfo->name : "unknown"));
+			ret = SR_ERR_NA;
+			break;
+		}
+	}
+		
 	return ret;
 }
 
 static int config_set(uint32_t key, GVariant *data,
 	const struct sr_dev_inst *sdi, const struct sr_channel_group *cg)
 {
+	struct dev_context *devc;
+	struct sr_channel *ch;
+	const struct sr_key_info *kinfo;
 	int ret;
+	
+	if (!sdi || !data)
+		return SR_ERR_ARG;
 
-	(void)sdi;
-	(void)data;
-	(void)cg;
-
+	devc = sdi->priv;
 	ret = SR_OK;
-	switch (key) {
-	/* TODO */
-	default:
-		ret = SR_ERR_NA;
-	}
+	kinfo = sr_key_info_get(SR_KEY_CONFIG, key);
 
+	if (!cg) {
+		switch (key) {
+		case SR_CONF_LIMIT_SAMPLES:
+		case SR_CONF_LIMIT_MSEC:
+			ret = sr_sw_limits_config_set(&devc->limits, key, data);
+			break;
+		default:
+			sr_dbg("%s: Unsupported key: %d (%s)", __func__,
+				(int)key, (kinfo ? kinfo->name : "unknown"));
+			ret = SR_ERR_NA;
+			break;
+		}
+	} else {
+		ch = cg->channels->data;
+
+		switch (key) {
+		case SR_CONF_ENABLED:
+			ret = mhs5200a_set_onoff(sdi, g_variant_get_boolean(data));
+			break;
+		case SR_CONF_PATTERN_MODE:
+			ret = mhs5200a_set_waveform_string(sdi, ch->index + 1, g_variant_get_string(data, NULL));
+			break;
+		case SR_CONF_OUTPUT_FREQUENCY:
+			ret = mhs5200a_set_frequency(sdi, ch->index + 1, g_variant_get_double(data));
+			break;
+		case SR_CONF_AMPLITUDE:
+			ret = mhs5200a_set_amplitude(sdi, ch->index + 1, g_variant_get_double(data));
+			break;
+		case SR_CONF_OFFSET:
+			ret = mhs5200a_set_offset(sdi, ch->index + 1, g_variant_get_double(data));
+			break;
+		case SR_CONF_PHASE:
+			ret = mhs5200a_set_phase(sdi, ch->index + 1, g_variant_get_double(data));
+			break;
+		case SR_CONF_DUTY_CYCLE:
+			ret = mhs5200a_set_duty_cycle(sdi, ch->index + 1, g_variant_get_double(data));
+			break;
+		default:
+			sr_dbg("%s: Unsupported (cg) key: %d (%s)", __func__,
+			       (int)key, (kinfo ? kinfo->name : "unknown"));
+			ret = SR_ERR_NA;
+			break;
+		}
+	}
+		
 	return ret;
 }
 
 static int config_list(uint32_t key, GVariant **data,
 	const struct sr_dev_inst *sdi, const struct sr_channel_group *cg)
 {
-	int ret;
+	struct dev_context *devc;
+	struct sr_channel *ch;
+	const struct channel_spec *ch_spec;
+	GVariantBuilder *b;
+	unsigned int i;
+	double fspec[3];
 
-	(void)sdi;
-	(void)data;
-	(void)cg;
-
-	ret = SR_OK;
-	switch (key) {
-	/* TODO */
-	default:
-		return SR_ERR_NA;
+	if (sdi) {
+		devc = sdi->priv;
+	} else {
+		devc = NULL;
+	}
+	
+	if (!cg) {
+		switch (key) {
+		case SR_CONF_SCAN_OPTIONS:
+		case SR_CONF_DEVICE_OPTIONS:
+			return std_opts_config_list(key, data, sdi, cg,
+						    ARRAY_AND_SIZE(scanopts),
+						    ARRAY_AND_SIZE(drvopts),
+						    ARRAY_AND_SIZE(mhs5200a_devopts));
+		default:
+			return SR_ERR_NA;
+		}
+	} else {
+		ch = cg->channels->data;
+		ch_spec = &mhs5200a_channels[ch->index];
+		switch(key) {
+		case SR_CONF_DEVICE_OPTIONS:
+			*data = std_gvar_array_u32(ARRAY_AND_SIZE(mhs5200a_devopts_cg));
+			break;
+		case SR_CONF_PATTERN_MODE:
+			b = g_variant_builder_new(G_VARIANT_TYPE("as"));
+			for (i = 0; i < ch_spec->num_waveforms; i++) {
+				g_variant_builder_add(b, "s",
+						      mhs5200a_waveform_to_string(ch_spec->waveforms[i].waveform));
+			}
+			*data = g_variant_new("as", b);
+			g_variant_builder_unref(b);
+			break;
+		case SR_CONF_OUTPUT_FREQUENCY:
+			if (!devc) {
+				return SR_ERR;
+			}
+			fspec[0] = 0.1;
+			fspec[1] = devc->max_frequency;
+			fspec[2] = 0.1;
+			*data = std_gvar_min_max_step_array(fspec);
+			break;
+		case SR_CONF_PHASE:
+			*data = std_gvar_min_max_step_array(phase_min_max_step);
+			break;
+		default:
+			return SR_ERR_NA;
+		}
 	}
 
-	return ret;
+	return SR_OK;
 }
 
 static int dev_acquisition_start(const struct sr_dev_inst *sdi)
 {
-	/* TODO: configure hardware, reset acquisition state, set up
-	 * callbacks and send header packet. */
+	struct dev_context *devc;
+	int ret;
+	
+	if (!sdi)
+		return SR_ERR_ARG;
 
-	(void)sdi;
 
-	return SR_OK;
+	ret = mhs5200a_set_counter_function(sdi, COUNTER_MEASURE_FREQUENCY);
+	if (ret < 0)
+		return SR_ERR;
+	
+	ret = mhs5200a_set_counter_onoff(sdi, 1);
+	if (ret < 0)
+		return SR_ERR;
+	
+	devc = sdi->priv;
+
+	sr_sw_limits_acquisition_start(&devc->limits);
+	ret = std_session_send_df_header(sdi);
+	sr_session_source_add(sdi->session, -1, 0, 1000, mhs5200a_receive_data, (void *)sdi);
+	return ret;
 }
 
 static int dev_acquisition_stop(struct sr_dev_inst *sdi)
 {
-	/* TODO: stop acquisition. */
+	if (!sdi)
+		return SR_ERR_ARG;
 
-	(void)sdi;
-
+	mhs5200a_set_counter_onoff(sdi, 0);
+	sr_session_source_remove(sdi->session, -1);
+	std_session_send_df_end(sdi);
 	return SR_OK;
 }
 
-static struct sr_dev_driver mhinstek_mhs_5200a_driver_info = {
+static struct sr_dev_driver mhinstek_mhs5200a_driver_info = {
 	.name = "mhinstek-mhs-5200a",
 	.longname = "MHINSTEK MHS-5200A",
 	.api_version = 1,
@@ -145,10 +423,17 @@ static struct sr_dev_driver mhinstek_mhs_5200a_driver_info = {
 	.config_get = config_get,
 	.config_set = config_set,
 	.config_list = config_list,
-	.dev_open = dev_open,
-	.dev_close = dev_close,
+	.dev_open = std_serial_dev_open,
+	.dev_close = std_serial_dev_close,
 	.dev_acquisition_start = dev_acquisition_start,
 	.dev_acquisition_stop = dev_acquisition_stop,
 	.context = NULL,
 };
-SR_REGISTER_DEV_DRIVER(mhinstek_mhs_5200a_driver_info);
+SR_REGISTER_DEV_DRIVER(mhinstek_mhs5200a_driver_info);
+
+/* Local Variables: */
+/* mode: c */
+/* indent-tabs-mode: t */
+/* c-basic-offset: 8 */
+/* tab-width: 8 */
+/* End: */
--- src/hardware/mhinstek-mhs-5200a/protocol.h
+++ src/hardware/mhinstek-mhs-5200a/protocol.h
@@ -17,8 +17,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef LIBSIGROK_HARDWARE_MHINSTEK_MHS_5200A_PROTOCOL_H
-#define LIBSIGROK_HARDWARE_MHINSTEK_MHS_5200A_PROTOCOL_H
+#ifndef LIBSIGROK_HARDWARE_MHINSTEK_MHS5200A_PROTOCOL_H
+#define LIBSIGROK_HARDWARE_MHINSTEK_MHS5200A_PROTOCOL_H
 
 #include <stdint.h>
 #include <glib.h>
@@ -27,9 +27,109 @@
 
 #define LOG_PREFIX "mhinstek-mhs-5200a"
 
-struct dev_context {
+#define PROTOCOL_LEN_MAX 32	/**< Max. line length for requests */
+#define SERIAL_READ_TIMEOUT_MS 50
+#define SERIAL_WRITE_TIMEOUT_MS 50
+
+// don't change the values, these are returned by the function generator
+enum attenuation_type {
+	ATTENUATION_MINUS_20DB = 0,
+	ATTENUATION_0DB        = 1,
 };
 
-SR_PRIV int mhinstek_mhs_5200a_receive_data(int fd, int revents, void *cb_data);
+// don't change the values, these are returned by the function generator
+enum waveform_type {
+	WAVEFORM_SINE = 0,
+	WAVEFORM_SQUARE,
+	WAVEFORM_TRIANGLE,
+	WAVEFORM_RISING_SAWTOOTH,
+	WAVEFORM_FALLING_SAWTOOTH,
+	WAVEFORM_UNKNOWN = 1000,
+};
+
+enum waveform_options {
+	WFO_FREQUENCY = 1,
+	WFO_AMPLITUDE = 2,
+	WFO_OFFSET = 4,
+	WFO_PHASE = 8,
+	WFO_DUTY_CYCLE = 16,
+};
+
+enum counter_function {
+	COUNTER_MEASURE_FREQUENCY = 0,
+	COUNTER_MEASURE_COUNT,
+	COUNTER_MEASURE_PERIOD,
+	COUNTER_MEASURE_PULSE_WIDTH,
+	COUNTER_MEASURE_NEGATIVE_PULSE_WIDTH,
+	COUNTER_MEASURE_DUTY_CYCLE,
+};
+
+enum gate_time {
+	COUNTER_GATE_TIME_1_SEC = 0,
+	COUNTER_GATE_TIME_10_SEC,
+	COUNTER_GATE_TIME_10_MSEC,
+	COUNTER_GATE_TIME_100_MSEC,
+};
+
+struct waveform_spec {
+	enum waveform_type waveform;
+	double freq_min;
+	double freq_max;
+	double freq_step;
+	uint32_t opts;
+};
+
+struct channel_spec {
+	const char *name;
+	const struct waveform_spec *waveforms;
+	uint32_t num_waveforms;
+};
+
+struct dev_context {
+	struct sr_sw_limits limits;
+	size_t buflen;
+	double max_frequency; // for sine wave, all others are 6MHz
+	uint8_t buf[PROTOCOL_LEN_MAX];
+
+};
+
+
+SR_PRIV int mhs5200a_receive_data(int fd, int revents, void *cb_data);
+SR_PRIV const char *mhs5200a_waveform_to_string(enum waveform_type type);
+SR_PRIV enum waveform_type mhs5200a_string_to_waveform(const char *type);
+SR_PRIV int mhs5200a_frequency_limits(enum waveform_type wtype, double *freq_min, double *freq_max);
+SR_PRIV int mhs5200a_get_model(struct sr_serial_dev_inst *serial, char *model, int modelsize);
+SR_PRIV int mhs5200a_get_waveform(const struct sr_dev_inst *sdi, int ch, long *val);
+SR_PRIV int mhs5200a_get_attenuation(const struct sr_dev_inst *sdi, int ch, long *val);
+SR_PRIV int mhs5200a_get_onoff(const struct sr_dev_inst *sdi, long *val);
+SR_PRIV int mhs5200a_get_frequency(const struct sr_dev_inst *sdi, int ch, double *val);
+SR_PRIV int mhs5200a_get_amplitude(const struct sr_dev_inst *sdi, int ch, double *val);
+SR_PRIV int mhs5200a_get_duty_cycle(const struct sr_dev_inst *sdi, int ch, double *val);
+SR_PRIV int mhs5200a_get_offset(const struct sr_dev_inst *sdi, int ch, double *val);
+SR_PRIV int mhs5200a_get_phase(const struct sr_dev_inst *sdi, int ch, double *val);
+SR_PRIV int mhs5200a_set_frequency(const struct sr_dev_inst *sdi, int ch, double val);
+SR_PRIV int mhs5200a_set_waveform(const struct sr_dev_inst *sdi, int ch, long val);
+SR_PRIV int mhs5200a_set_waveform_string(const struct sr_dev_inst *sdi, int ch, const char *val);
+SR_PRIV int mhs5200a_set_amplitude(const struct sr_dev_inst *sdi, int ch, double val);
+SR_PRIV int mhs5200a_set_duty_cycle(const struct sr_dev_inst *sdi, int ch, double val);
+SR_PRIV int mhs5200a_set_offset(const struct sr_dev_inst *sdi, int ch, double val);
+SR_PRIV int mhs5200a_set_phase(const struct sr_dev_inst *sdi, int ch, double val);
+SR_PRIV int mhs5200a_set_attenuation(const struct sr_dev_inst *sdi, int ch, long val);
+SR_PRIV int mhs5200a_set_onoff(const struct sr_dev_inst *sdi, long val);
+SR_PRIV int mhs5200a_set_counter_onoff(const struct sr_dev_inst *sdi, long val);
+SR_PRIV int mhs5200a_set_counter_function(const struct sr_dev_inst *sdi, enum counter_function val);
+SR_PRIV int mhs5200a_set_counter_gate_time(const struct sr_dev_inst *sdi, enum gate_time val);
+SR_PRIV int mhs5200a_get_counter_value(const struct sr_dev_inst *sdi, double *val);
+SR_PRIV int mhs5200a_get_counter_frequency(const struct sr_dev_inst *sdi, double *val);
+SR_PRIV int mhs5200a_get_counter_period(const struct sr_dev_inst *sdi, double *val);
+SR_PRIV int mhs5200a_get_counter_pulse_width(const struct sr_dev_inst *sdi, double *val);
+SR_PRIV int mhs5200a_get_counter_duty_cycle(const struct sr_dev_inst *sdi, double *val);
 
 #endif
+
+/* Local Variables: */
+/* mode: c */
+/* indent-tabs-mode: t */
+/* c-basic-offset: 8 */
+/* tab-width: 8 */
+/* End: */
