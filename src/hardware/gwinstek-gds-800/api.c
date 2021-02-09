0529be70bd71bbf66801d04e046d1955d9eb9491
Richard Allen <rsaxvc@rsaxvc.net>
2021-02-08 20:10:13 -0600

    gwinstek-gds800: handle dirty GDS RX buffer
    Send the identification request a few times in
    case the GDS receive buffer is dirty. This
    can happen by unplugging, terminating a
    previous libsigrok session, or ModemManager
    interference.

--- src/hardware/gwinstek-gds-800/api.c
+++ src/hardware/gwinstek-gds-800/api.c
@@ -22,6 +22,8 @@
 #include <math.h>
 #include "protocol.h"
 
+#define IDN_RETRIES 3 /* at least 2 */
+
 static const uint32_t scanopts[] = {
 	SR_CONF_CONN,
 	SR_CONF_SERIALCOMM,
@@ -79,7 +81,17 @@ static struct sr_dev_inst *probe_device(struct sr_scpi_dev_inst *scpi, struct sr
 	struct sr_scpi_hw_info *hw_info;
 	int i;
 
-	if (sr_scpi_get_hw_id(scpi, &hw_info) != SR_OK) {
+	/*
+	 * If there is already data in the GDS receive buffer,
+	 * the first SCPI IDN may fail, so try a few times
+	 */
+	for (i = 0; i < IDN_RETRIES; i++) {
+		/* Request the GDS to identify itself */
+		if (sr_scpi_get_hw_id(scpi, &hw_info) == SR_OK)
+			break;
+	}
+
+	if (i == IDN_RETRIES) {
 		sr_info("Couldn't get IDN response.");
 		return NULL;
 	}
