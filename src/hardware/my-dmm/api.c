f0b9bbab0bd1965d6240208d4c2e0974da93400c
AlexEagleC <orlovaleksandr7922@gmail.com>
2021-04-04 23:27:16 +0300

    + dmm(doesn't works for long)

--- src/hardware/my-dmm/api.c
+++ src/hardware/my-dmm/api.c
@@ -18,25 +18,147 @@
  */
 
 #include <config.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
 #include "protocol.h"
 
-static struct sr_dev_driver my_dmm_driver_info;
+#define SERIALCOMM "9600/8n1"
+
+static const uint32_t scanopts[] = {
+	SR_CONF_CONN,
+	SR_CONF_SERIALCOMM,
+};
+
+static const uint32_t drvopts[] = {
+	SR_CONF_MULTIMETER
+};
+
+static const uint32_t devopts[] = {
+	SR_CONF_MEASURED_QUANTITY | SR_CONF_SET,
+	SR_CONF_RANGE | SR_CONF_SET,
+	SR_CONF_ENABLED | SR_CONF_SET,
+	SR_CONF_CONTINUOUS,
+	SR_CONF_LIMIT_SAMPLES | SR_CONF_GET | SR_CONF_SET,
+	SR_CONF_LIMIT_MSEC | SR_CONF_GET | SR_CONF_SET,
+	SR_CONF_SAMPLERATE | SR_CONF_GET | SR_CONF_SET | SR_CONF_LIST,
+	SR_CONF_DATA_SOURCE | SR_CONF_GET | SR_CONF_SET | SR_CONF_LIST,
+};
+
+/*static const uint32_t devopts_cg[] = {
+	SR_CONF_ENABLED | SR_CONF_SET,
+	SR_CONF_REGULATION | SR_CONF_GET | SR_CONF_LIST,
+	SR_CONF_VOLTAGE | SR_CONF_GET,
+	SR_CONF_CURRENT | SR_CONF_GET
+}*/
+
+static struct sr_dev_driver example_driver_info;
+
+#define RESPONSE_DELAY_US (20 * 1000)
+
+static const uint64_t samplerates[] = {
+	SR_HZ(1),
+	SR_HZ(20),
+	SR_HZ(1),
+};
+
+static const char *data_sources[] = {
+	"Live", "Log-Hand", "Log-Trig", "Log-Auto", "Log-Export",
+};
 
 static GSList *scan(struct sr_dev_driver *di, GSList *options)
 {
-	struct drv_context *drvc;
-	GSList *devices;
-
-	(void)options;
+	struct sr_dev_inst *sdi;
+	struct dev_context *devc;
+	struct sr_config *src;
+	struct sr_serial_dev_inst *serial;
+	struct sr_channel_group *cg;
+	struct sr_channel *ch;
+	GSList *l, *devices;
+	int num_read;
+	int len;
+	const char *conn, *serialcomm;
+	char *buf, **tokens;
 
 	devices = NULL;
-	drvc = di->context;
-	drvc->instances = NULL;
+	conn = serialcomm = NULL;
 
-	/* TODO: scan for devices, either based on a SR_CONF_CONN option
-	 * or on a USB scan. */
 
-	return devices;
+	for (l = options; l; l = l->next) {
+		src = l->data;
+		switch (src->key) {
+		case SR_CONF_CONN:
+			conn = g_variant_get_string(src->data, NULL);
+			break;
+		case SR_CONF_SERIALCOMM:
+			serialcomm = g_variant_get_string(src->data, NULL);
+			break;
+		}
+	}
+	if (!conn)
+		return NULL;
+	if (!serialcomm)
+		serialcomm = SERIALCOMM;
+
+	serial = sr_serial_dev_inst_new(conn, serialcomm);
+
+	if (serial_open(serial, SERIAL_RDWR) != SR_OK)
+		return NULL;
+
+	buf = g_malloc0(sizeof(uint8_t));
+	*buf = CMD_SCAN;
+
+	if (serial_write_blocking(serial, buf, 1, 100) != 1) {
+		sr_err("Unable to send identification string.");
+		
+		return NULL;
+	}
+	
+	len = 64;
+	buf = g_malloc(len);
+	serial_readline(serial, &buf, &len, 100);
+
+	tokens = g_strsplit(buf, ",", 3);
+
+	if (tokens[0] && tokens[1] && tokens[2]) {
+		sdi = g_malloc0(sizeof(struct sr_dev_inst));
+		sdi->status = SR_ST_INACTIVE;
+		sdi->vendor = g_strdup(tokens[0]);
+		sdi->model = g_strdup(tokens[1]);
+		sdi->version = g_strdup(tokens[2]);
+
+		cg = g_malloc0(sizeof(struct sr_channel_group));
+	cg->name = g_strdup("1");
+	sdi->channel_groups = g_slist_append(sdi->channel_groups, cg);
+
+	ch = sr_channel_new(sdi, 0, SR_CHANNEL_ANALOG, TRUE, "V");
+	cg->channels = g_slist_append(cg->channels, ch);
+
+	ch = sr_channel_new(sdi, 1, SR_CHANNEL_ANALOG, TRUE, "I");
+	cg->channels = g_slist_append(cg->channels, ch);
+
+
+		devc = g_malloc0(sizeof(struct dev_context));
+		sr_sw_limits_init(&devc->limits);
+		devc->cur_mq[0] = SR_MQ_VOLTAGE;
+		devc->cur_mq[1] = SR_MQ_CURRENT;
+		devc->data_source = 0;
+		devc->cur_samplerate = 1;
+		sdi->inst_type = SR_INST_SERIAL;
+		sdi->conn = serial;
+		sdi->priv = devc;
+
+		devices = g_slist_append(devices, sdi);
+	}
+	g_strfreev(tokens);
+	g_free(buf);
+
+	serial_close(serial);
+	if (!devices)
+		sr_serial_dev_inst_free(serial);
+
+	return std_scan_complete(di, devices);
 }
 
 static int dev_open(struct sr_dev_inst *sdi)
@@ -60,38 +182,45 @@ static int dev_close(struct sr_dev_inst *sdi)
 static int config_get(uint32_t key, GVariant **data,
 	const struct sr_dev_inst *sdi, const struct sr_channel_group *cg)
 {
-	int ret;
+	struct dev_context *devc;
 
-	(void)sdi;
-	(void)data;
 	(void)cg;
 
-	ret = SR_OK;
+	devc = sdi->priv;
 	switch (key) {
-	/* TODO */
+	case SR_CONF_SAMPLERATE:
+		*data = g_variant_new_uint64(devc->cur_samplerate);
+		break;
+	case SR_CONF_LIMIT_SAMPLES:
+	case SR_CONF_LIMIT_MSEC:
+		return sr_sw_limits_config_get(&devc->limits, key, data);
+	case SR_CONF_DATA_SOURCE:
+		*data = g_variant_new_string(data_sources[devc->data_source]);
+		break;
 	default:
 		return SR_ERR_NA;
 	}
 
-	return ret;
+	return SR_OK;
 }
 
 static int config_set(uint32_t key, GVariant *data,
 	const struct sr_dev_inst *sdi, const struct sr_channel_group *cg)
 {
 	int ret;
-
+	struct dev_context *devc;
 	(void)sdi;
 	(void)data;
 	(void)cg;
-
+	devc = sdi->priv;
 	ret = SR_OK;
 	switch (key) {
-	/* TODO */
+		case SR_CONF_LIMIT_SAMPLES:
+		case SR_CONF_LIMIT_MSEC:
+			return sr_sw_limits_config_set(&devc->limits, key, data);
 	default:
 		ret = SR_ERR_NA;
 	}
-
 	return ret;
 }
 
@@ -103,10 +232,17 @@ static int config_list(uint32_t key, GVariant **data,
 	(void)sdi;
 	(void)data;
 	(void)cg;
-
 	ret = SR_OK;
 	switch (key) {
-	/* TODO */
+	case SR_CONF_SCAN_OPTIONS:
+	case SR_CONF_DEVICE_OPTIONS:
+		return STD_CONFIG_LIST(key, data, sdi, cg, scanopts, drvopts, devopts);
+	case SR_CONF_SAMPLERATE:
+		*data = std_gvar_samplerates_steps(ARRAY_AND_SIZE(samplerates));
+		break;
+	case SR_CONF_DATA_SOURCE:
+		*data = g_variant_new_strv(ARRAY_AND_SIZE(data_sources));
+		break;
 	default:
 		return SR_ERR_NA;
 	}
@@ -121,21 +257,51 @@ static int dev_acquisition_start(const struct sr_dev_inst *sdi)
 
 	(void)sdi;
 
+	struct dev_context *devc = sdi->priv;
+	struct sr_serial_dev_inst *serial;
+	char *buf = g_malloc0(sizeof(uint8_t));
+	*buf = CMD_START;
+	serial = sdi->conn;
+	if (serial_write_blocking(serial, buf, 1, 1) != 1) {
+		sr_err("Unable to send identification string.");
+		
+		return NULL;
+	}
+	//sr_dev_acquisition_stop(sdi);
+	sr_sw_limits_acquisition_start(&devc->limits);
+	devc->limits.limit_samples = 5000;
+	devc->limits.limit_frames = 5000;
+	//devc->limits.limit_msec = 100000;
+	std_session_send_df_header(sdi);
+
+	memset(devc->buf, 0, BUFSIZE);
+	devc->buflen = 0;
+	
+	g_mutex_init(&devc->acquisition_mutex);
+
+	serial_source_add(sdi->session, serial, G_IO_IN, 100,
+			my_dmm_receive_data, (void *)sdi);
+
 	return SR_OK;
 }
 
 static int dev_acquisition_stop(struct sr_dev_inst *sdi)
 {
 	/* TODO: stop acquisition. */
+	struct dev_context *devc;
+	int ret;
 
-	(void)sdi;
+	devc = sdi->priv;
 
-	return SR_OK;
+	ret = std_serial_dev_acquisition_stop(sdi);
+	g_mutex_clear(&devc->acquisition_mutex);
+
+	return ret;
 }
 
-static struct sr_dev_driver my_dmm_driver_info = {
-	.name = "my-dmm",
-	.longname = "My_dmm",
+static struct sr_dev_driver example_driver_info = {
+	.name = "Example",
+	.longname = "Example for sigrok",
 	.api_version = 1,
 	.init = std_init,
 	.cleanup = std_cleanup,
@@ -145,10 +311,10 @@ static struct sr_dev_driver my_dmm_driver_info = {
 	.config_get = config_get,
 	.config_set = config_set,
 	.config_list = config_list,
-	.dev_open = dev_open,
-	.dev_close = dev_close,
+	.dev_open = std_serial_dev_open,
+	.dev_close = std_serial_dev_close,
 	.dev_acquisition_start = dev_acquisition_start,
 	.dev_acquisition_stop = dev_acquisition_stop,
 	.context = NULL,
 };
-SR_REGISTER_DEV_DRIVER(my_dmm_driver_info);
+SR_REGISTER_DEV_DRIVER(example_driver_info);
--- src/hardware/my-dmm/protocol.h
+++ src/hardware/my-dmm/protocol.h
@@ -17,17 +17,37 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef LIBSIGROK_HARDWARE_MY_DMM_PROTOCOL_H
-#define LIBSIGROK_HARDWARE_MY_DMM_PROTOCOL_H
+#ifndef LIBSIGROK_HARDWARE_EXAMPLE_PROTOCOL_H
+#define LIBSIGROK_HARDWARE_EXAMPLE_PROTOCOL_H
 
 #include <stdint.h>
 #include <glib.h>
 #include <libsigrok/libsigrok.h>
 #include "libsigrok-internal.h"
 
-#define LOG_PREFIX "my-dmm"
+#define LOG_PREFIX "example"
+
+#define CMD_CONF 0x00
+#define CMD_SCAN 0x01
+#define CMD_START 0x02
+
+#define MAX_CHANNELS 2
+#define BUFSIZE 100
 
 struct dev_context {
+    struct sr_sw_limits limits;
+    int data_source;
+    uint64_t cur_samplerate;
+    int cur_mq[MAX_CHANNELS];
+
+    char buf[BUFSIZE];
+	int buflen;
+
+    GMutex acquisition_mutex;
+
+	float current_limit;
+	float voltage;
+	float current;
 };
 
 SR_PRIV int my_dmm_receive_data(int fd, int revents, void *cb_data);
