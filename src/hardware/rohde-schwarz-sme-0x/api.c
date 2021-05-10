d4c027e2bfdaf02cfb90c956e24b9d92e85c6b09
Daniel Anselmi <danselmi@gmx.ch>
2021-05-11 01:53:04 +0200

    rohde-schwarz-sme-0x: add support for SMB100A, SMBV100A and SMC100A

--- src/hardware/rohde-schwarz-sme-0x/api.c
+++ src/hardware/rohde-schwarz-sme-0x/api.c
@@ -2,6 +2,7 @@
  * This file is part of the libsigrok project.
  *
  * Copyright (C) 2016 Vlad Ivanov <vlad.ivanov@lab-systems.ru>
+ * Copyright (C) 2021 Daniel Anselmi <danselmi@gmx.ch>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -27,42 +28,55 @@ static struct sr_dev_driver rohde_schwarz_sme_0x_driver_info;
 
 static const char *manufacturer = "Rohde&Schwarz";
 
+static const struct rs_device_model_config model_sme0x =
+{
+	.freq_step = 0.1,
+	.power_step = 0.1,
+	.commands = commands_sme0x,
+	.responses = responses_sme0x,
+};
+
+static const struct rs_device_model_config model_smx100 =
+{
+	.freq_step = 0.001,
+	.power_step = 0.01,
+	.commands = commands_smx100,
+	.responses = responses_smx100,
+};
+
 static const struct rs_device_model device_models[] = {
 	{
 		.model_str = "SME02",
-		.freq_max = SR_GHZ(1.5),
-		.freq_min = SR_KHZ(5),
-		.power_max = 16,
-		.power_min = -144,
+		.model_config = &model_sme0x,
 	},
 	{
 		.model_str = "SME03E",
-		.freq_max = SR_GHZ(2.2),
-		.freq_min = SR_KHZ(5),
-		.power_max = 16,
-		.power_min = -144,
+		.model_config = &model_sme0x,
 	},
 	{
 		.model_str = "SME03A",
-		.freq_max = SR_GHZ(3),
-		.freq_min = SR_KHZ(5),
-		.power_max = 16,
-		.power_min = -144,
+		.model_config = &model_sme0x,
 	},
 	{
 		.model_str = "SME03",
-		.freq_max = SR_GHZ(3),
-		.freq_min = SR_KHZ(5),
-		.power_max = 16,
-		.power_min = -144,
+		.model_config = &model_sme0x,
 	},
 	{
 		.model_str = "SME06",
-		.freq_max = SR_GHZ(1.5),
-		.freq_min = SR_KHZ(5),
-		.power_max = 16,
-		.power_min = -144,
-	}
+		.model_config = &model_sme0x,
+	},
+	{
+		.model_str = "SMB100A",
+		.model_config = &model_smx100,
+	},
+	{
+		.model_str = "SMBV100A",
+		.model_config = &model_smx100,
+	},
+	{
+		.model_str = "SMC100A",
+		.model_config = &model_smx100,
+	},
 };
 
 static const uint32_t scanopts[] = {
@@ -71,18 +85,27 @@ static const uint32_t scanopts[] = {
 };
 
 static const uint32_t drvopts[] = {
-        SR_CONF_SIGNAL_GENERATOR,
+	SR_CONF_SIGNAL_GENERATOR,
 };
 
 static const uint32_t devopts[] = {
-	SR_CONF_OUTPUT_FREQUENCY | SR_CONF_GET | SR_CONF_SET | SR_CONF_LIST,
-	SR_CONF_AMPLITUDE | SR_CONF_GET | SR_CONF_SET | SR_CONF_LIST,
+	SR_CONF_OUTPUT_FREQUENCY      | SR_CONF_GET | SR_CONF_SET | SR_CONF_LIST,
+	SR_CONF_AMPLITUDE             | SR_CONF_GET | SR_CONF_SET | SR_CONF_LIST,
+	SR_CONF_ENABLED               | SR_CONF_GET | SR_CONF_SET,
+	SR_CONF_EXTERNAL_CLOCK_SOURCE | SR_CONF_GET | SR_CONF_SET | SR_CONF_LIST,
+};
+
+static const char *clock_sources[] = {
+	"Internal",
+	"External",
 };
 
 static int rs_init_device(struct sr_dev_inst *sdi)
 {
 	struct dev_context *devc;
 	uint8_t model_found;
+	double min_val;
+	double max_val;
 
 	devc = sdi->priv;
 	model_found = 0;
@@ -90,7 +113,7 @@ static int rs_init_device(struct sr_dev_inst *sdi)
 	for (size_t i = 0; i < ARRAY_SIZE(device_models); i++) {
 		if (!strcmp(device_models[i].model_str, sdi->model)) {
 			model_found = 1;
-			devc->model_config = &device_models[i];
+			devc->model_config = device_models[i].model_config;
 			break;
 		}
 	}
@@ -100,6 +123,22 @@ static int rs_init_device(struct sr_dev_inst *sdi)
 			manufacturer, sdi->model);
 		return SR_ERR_NA;
 	}
+
+	if (rs_sme0x_init(sdi) != SR_OK)
+		return SR_ERR;
+
+	if (rs_sme0x_get_minmax_freq(sdi, &min_val, &max_val) != SR_OK)
+		return SR_ERR;
+	devc->freq_min = min_val;
+	devc->freq_max = max_val;
+
+	if (rs_sme0x_get_minmax_power(sdi, &min_val, &max_val) != SR_OK)
+		return SR_ERR;
+	devc->power_min = min_val;
+	devc->power_max = max_val;
+
+	if (rs_sme0x_sync(sdi) != SR_OK)
+		return SR_ERR;
 
 	return SR_OK;
 }
@@ -113,8 +152,6 @@ static struct sr_dev_inst *probe_device(struct sr_scpi_dev_inst *scpi)
 	sdi = NULL;
 	devc = NULL;
 	hw_info = NULL;
-
-	rs_sme0x_mode_remote(scpi);
 
 	if (sr_scpi_get_hw_id(scpi, &hw_info) != SR_OK)
 		goto fail;
@@ -156,11 +193,17 @@ static GSList *scan(struct sr_dev_driver *di, GSList *options)
 
 static int dev_open(struct sr_dev_inst *sdi)
 {
-	return sr_scpi_open(sdi->conn);
+	int ret;
+	if ((ret = sr_scpi_open(sdi->conn)) != SR_OK)
+		return ret;
+
+	ret = rs_sme0x_mode_remote(sdi);
+	return ret;
 }
 
 static int dev_close(struct sr_dev_inst *sdi)
 {
+	rs_sme0x_mode_local(sdi);
 	return sr_scpi_close(sdi->conn);
 }
 
@@ -168,10 +211,20 @@ static int config_get(uint32_t key, GVariant **data,
 	const struct sr_dev_inst *sdi, const struct sr_channel_group *cg)
 {
 	double value_f;
+	int idx;
+	gboolean bval;
+	struct dev_context *devc;
 
 	(void) cg;
 
+	if (!sdi || !(devc = sdi->priv))
+		return SR_ERR_ARG;
+
 	switch (key) {
+	case SR_CONF_ENABLED:
+		rs_sme0x_get_enable(sdi, &bval);
+		*data = g_variant_new_boolean(bval);
+		break;
 	case SR_CONF_OUTPUT_FREQUENCY:
 		rs_sme0x_get_freq(sdi, &value_f);
 		*data = g_variant_new_double(value_f);
@@ -179,6 +232,10 @@ static int config_get(uint32_t key, GVariant **data,
 	case SR_CONF_AMPLITUDE:
 		rs_sme0x_get_power(sdi, &value_f);
 		*data = g_variant_new_double(value_f);
+		break;
+	case SR_CONF_EXTERNAL_CLOCK_SOURCE:
+		rs_sme0x_get_clk_src_idx(sdi, &idx);
+		*data = g_variant_new_string(clock_sources[idx]);
 		break;
 	default:
 		return SR_ERR_NA;
@@ -190,33 +247,71 @@ static int config_get(uint32_t key, GVariant **data,
 static int config_set(uint32_t key, GVariant *data,
 	const struct sr_dev_inst *sdi, const struct sr_channel_group *cg)
 {
-	double value_f;
-
 	(void)cg;
+	int ret;
+	size_t i;
+	const char *clksrc_str;
 
 	if (!sdi)
 		return SR_ERR_ARG;
 
+	ret = SR_OK;
 	switch (key) {
+	case SR_CONF_ENABLED:
+		ret = rs_sme0x_set_enable(sdi, g_variant_get_boolean(data));
+		break;
 	case SR_CONF_OUTPUT_FREQUENCY:
-		value_f = g_variant_get_double(data);
-		rs_sme0x_set_freq(sdi, value_f);
+		ret = rs_sme0x_set_freq(sdi, g_variant_get_double(data));
 		break;
 	case SR_CONF_AMPLITUDE:
-		value_f = g_variant_get_double(data);
-		rs_sme0x_set_power(sdi, value_f);
+		ret = rs_sme0x_set_power(sdi, g_variant_get_double(data));
+		break;
+	case SR_CONF_EXTERNAL_CLOCK_SOURCE:
+		clksrc_str = g_variant_get_string(data, NULL);
+		for (i = 0; i < ARRAY_SIZE(clock_sources); i++)
+			if (g_strcmp0(clock_sources[i], clksrc_str) == 0) {
+				ret = rs_sme0x_set_clk_src(sdi, i);
+				break;
+			}
 		break;
 	default:
 		return SR_ERR_NA;
 	}
 
-	return SR_OK;
+	return ret;
 }
 
 static int config_list(uint32_t key, GVariant **data,
 	const struct sr_dev_inst *sdi, const struct sr_channel_group *cg)
 {
-	return STD_CONFIG_LIST(key, data, sdi, cg, scanopts, drvopts, devopts);
+	struct dev_context *devc;
+	devc = (sdi) ? sdi->priv : NULL;
+
+	switch (key) {
+	case SR_CONF_SCAN_OPTIONS:
+	case SR_CONF_DEVICE_OPTIONS:
+		return STD_CONFIG_LIST(key, data, sdi, cg, scanopts, drvopts, devopts);
+	case SR_CONF_AMPLITUDE:
+		if (!devc)
+			return SR_ERR_ARG;
+		*data = std_gvar_min_max_step(devc->power_min,
+									  devc->power_max,
+									  devc->model_config->power_step);
+		break;
+	case SR_CONF_OUTPUT_FREQUENCY:
+		if (!devc)
+			return SR_ERR_ARG;
+		*data = std_gvar_min_max_step(devc->freq_min,
+									devc->freq_max,
+									devc->model_config->freq_step);
+		break;
+	case SR_CONF_EXTERNAL_CLOCK_SOURCE:
+		*data = std_gvar_array_str(ARRAY_AND_SIZE(clock_sources));
+		break;
+	default:
+		return SR_ERR_NA;
+	}
+	return SR_OK;
 }
 
 static struct sr_dev_driver rohde_schwarz_sme_0x_driver_info = {
@@ -237,4 +332,6 @@ static struct sr_dev_driver rohde_schwarz_sme_0x_driver_info = {
 	.dev_acquisition_stop = std_serial_dev_acquisition_stop,
 	.context = NULL,
 };
+
 SR_REGISTER_DEV_DRIVER(rohde_schwarz_sme_0x_driver_info);
+
