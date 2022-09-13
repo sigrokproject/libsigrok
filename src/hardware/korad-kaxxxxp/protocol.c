sigrokproject/libsigrok

commit 5dfa77b54a45b7a767b03c4dce41ea41e62816eb

korad-kaxxxxp: silence compiler warning (unused value)
Do inspect the return value of the routine which sends the command
before retrieving the response. This silences the following warnings:

../src/hardware/korad-kaxxxxp/protocol.c:179:3: warning: Value stored to 'ret' is never read
                ret = korad_kaxxxxp_send_cmd(serial, "IOUT1?");
../src/hardware/korad-kaxxxxp/protocol.c:184:3: warning: Value stored to 'ret' is never read
                ret = korad_kaxxxxp_send_cmd(serial, "ISET1?");
../src/hardware/korad-kaxxxxp/protocol.c:189:3: warning: Value stored to 'ret' is never read
                ret = korad_kaxxxxp_send_cmd(serial, "VOUT1?");
../src/hardware/korad-kaxxxxp/protocol.c:194:3: warning: Value stored to 'ret' is never read
                ret = korad_kaxxxxp_send_cmd(serial, "VSET1?");
../src/hardware/korad-kaxxxxp/protocol.c:202:3: warning: Value stored to 'ret' is never read
                ret = korad_kaxxxxp_send_cmd(serial, "STATUS?");

src/hardware/korad-kaxxxxp/protocol.c
@@ -297,8 +297,11 @@ SR_PRIV int korad_kaxxxxp_get_value(struct sr_serial_dev_inst *serial,
 		break;
 	default:
 		sr_err("Don't know how to query %d.", target);
+		ret = SR_ERR;
+	}
+	if (ret != SR_OK) {
 		g_mutex_unlock(&devc->rw_mutex);
-		return SR_ERR;
+		return ret;
 	}
 
 	devc->req_sent_at = g_get_monotonic_time();

commit ea9cbb6c2f2d73680ed118c4d74e7b590190ab29

korad-kaxxxxp: All received values are zero
Commit 5dfa77b5 introduced a regression in korad_kaxxxxp_get_value().
Positive non-zero return values from command transmission made the
routine fail for every invocation. Fixup the error check and advance
to response reception, do provide the values retrieved from the PSU.

[ gsi: rephrase check, reword commit message ]

src/hardware/korad-kaxxxxp/protocol.c
@@ -299,7 +299,7 @@ SR_PRIV int korad_kaxxxxp_get_value(struct sr_serial_dev_inst *serial,
 		sr_err("Don't know how to query %d.", target);
 		ret = SR_ERR;
 	}
-	if (ret != SR_OK) {
+	if (ret < 0) {
 		g_mutex_unlock(&devc->rw_mutex);
 		return ret;
 	}