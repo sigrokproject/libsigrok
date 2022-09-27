d6202f2376eb18d818b2106ed1862cdbc1fc8c5a
François Revol <revol@free.fr>
2022-09-28 01:26:14 +0200

    dmm/bm85x: fix broken URLs

--- src/dmm/bm85x.c
+++ src/dmm/bm85x.c
@@ -24,9 +24,9 @@
  *
  * Protocol parser for Brymen BM850s DMM packets. The USB protocol (for the
  * cable) and the packet description (for the meter) were retrieved from:
- * http://brymen.com/product-html/Download2.html
- * http://brymen.com/product-html/PD02BM850s_protocolDL.html
- * http://brymen.com/product-html/images/DownloadList/ProtocolList/BM850-BM850a-BM850s_List/BM850-BM850a-BM850s-500000-count-DMM-protocol-BC85X-BC85Xa.zip
+ * http://www.brymen.com/Download2.html
+ * http://www.brymen.com/PD02BM850s_protocolDL.html
+ * http://www.brymen.com/images/DownloadList/ProtocolList/BM850-BM850a-BM850s_List/BM850-BM850a-BM850s-500000-count-DMM-protocol-BC85X-BC85Xa.zip
  *
  * Implementor's notes on the protocol:
  * - The BM85x devices require a low RTS pulse after COM port open and
