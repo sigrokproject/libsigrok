9a172f9007ddce6f00abc4245636c44a86f0837b
Mathieu Pilato <pilato.mathieu@free.fr>
2023-03-09 16:34:07 +0100

    fluke-dmm: Add support for Fluke 87/89 DMMs.

--- src/hardware/fluke-dmm/protocol.c
+++ src/hardware/fluke-dmm/protocol.c
@@ -58,7 +58,7 @@ static void handle_qm_18x(const struct sr_dev_inst *sdi, char **tokens)
 		*e++ = '\0';
 		if (sr_atof_ascii(tokens[1], &fvalue) != SR_OK || fvalue == 0.0) {
 			/* Happens all the time, when switching modes. */
-			sr_dbg("Invalid float.");
+			sr_dbg("Invalid float : '%s'", tokens[1]);
 			return;
 		}
 	}
@@ -443,7 +443,8 @@ static void handle_line(const struct sr_dev_inst *sdi)
 
 	tokens = g_strsplit(devc->buf, ",", 0);
 	if (tokens[0]) {
-		if (devc->profile->model == FLUKE_187 || devc->profile->model == FLUKE_189) {
+		if (devc->profile->model == FLUKE_87 || devc->profile->model == FLUKE_89 ||
+				devc->profile->model == FLUKE_187 || devc->profile->model == FLUKE_189) {
 			devc->expect_response = FALSE;
 			handle_qm_18x(sdi, tokens);
 		} else if (devc->profile->model == FLUKE_287 || devc->profile->model == FLUKE_289) {
