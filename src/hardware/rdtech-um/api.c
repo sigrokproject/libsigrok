8ff25aa9b285edca280b2abc95382a8a5ba785d6
Gerhard Sittig <gerhard.sittig@gmx.net>
2023-03-15 17:55:19 +0100

    rdtech-um: use common helper to parse conn= and serialcomm= specs
    Use the common sr_serial_extract_options() helper when conn= specs
    are mandatory (and serialcomm= remains optional).

--- src/hardware/rdtech-um/api.c
+++ src/hardware/rdtech-um/api.c
@@ -102,22 +102,10 @@ static GSList *scan(struct sr_dev_driver *di, GSList *options)
 {
 	const char *conn;
 	const char *serialcomm;
-	GSList *l;
-	struct sr_config *src;
 
 	conn = NULL;
 	serialcomm = RDTECH_UM_SERIALCOMM;
-	for (l = options; l; l = l->next) {
-		src = l->data;
-		switch (src->key) {
-		case SR_CONF_CONN:
-			conn = g_variant_get_string(src->data, NULL);
-			break;
-		case SR_CONF_SERIALCOMM:
-			serialcomm = g_variant_get_string(src->data, NULL);
-			break;
-		}
-	}
+	(void)sr_serial_extract_options(options, &conn, &serialcomm);
 	if (!conn)
 		return NULL;
 
