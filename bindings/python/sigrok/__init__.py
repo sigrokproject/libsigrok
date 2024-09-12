6669e08e02cc0cf65d292275cf40c822ea3d0048
Marc Schink <dev@zapb.de>
2024-09-12 14:18:20 +0200

    bindings/python: Use implicit namespaces
    The use of 'namespace_packages' is deprecated [1] and for Python >= 3.3,
    implicit/native namespaces should be used. There are no incompatibilities
    as we require Python >= 3.8. More details can be found in [2] and [3].
    
    [1] https://setuptools.pypa.io/en/latest/references/keywords.html#keyword-namespace-packages
    [2] https://packaging.python.org/en/latest/guides/packaging-namespace-packages/
    [3] https://peps.python.org/pep-0420/

--- bindings/python/sigrok/__init__.py
+++ bindings/python/sigrok/__init__.py
@@ -16,5 +16,3 @@
 ## You should have received a copy of the GNU General Public License
 ## along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ##
-
-__import__("pkg_resources").declare_namespace(__name__)
